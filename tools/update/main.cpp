c9c750f4b2c63318661b3fb095dd31cf0f2281a7
Eren Okka <erenokka@gmail.com>
2012-07-22 17:07:45 +0000

    An attempt to fix issue 7.
    git-svn-id: http://taiga.googlecode.com/svn/trunk@166 77add795-4bdf-f0f6-a574-f4d068491005

--- tools/update/main.cpp
+++ tools/update/main.cpp
@@ -1,5 +1,5 @@
 /*
-** Copyright (c) 2011, Eren Okka
+** Copyright (c) 2011-2012, Eren Okka
 ** 
 ** Permission is hereby granted, free of charge, to any person obtaining a copy
 ** of this software and associated documentation files (the "Software"), to deal
@@ -22,9 +22,6 @@
 
 #include <windows.h>
 
-// Usage example:
-// UpdateHelper.exe App.exe App.exe.new [Process ID]
-
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nShowCmd) {
   // argv[0]: Application path (not used)
   // argv[1]: Old file
@@ -32,44 +29,46 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
   // argv[3]: Process ID (optional)
   int argc = 0;
   LPWSTR* argv = CommandLineToArgvW(GetCommandLineW(), &argc);
-  if (argc < 3 || argv == NULL) {
-    MessageBoxW(NULL, L"Usage example:\nUpdateHelper.exe App.exe App.exe.new [Process ID]", NULL, MB_ICONERROR | MB_OK);
+  if (argc < 3 || argv == nullptr) {
+    MessageBoxW(nullptr, L"Usage example:\n"
+                L"UpdateHelper.exe App.exe App.exe.new [Process ID]", 
+                nullptr, MB_ICONERROR | MB_OK);
     return 1;
   }
 
   // Wait for the application to close
   DWORD dwProcessId = argc > 3 ? _wtoi(argv[3]) : 0;
   if (dwProcessId) {
-    // Wait until the process is no longer available
-    HANDLE hProcess = NULL;
-    do {
-      hProcess = OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, dwProcessId);
-      if (hProcess) {
-        CloseHandle(hProcess);
-        Sleep(100);
+    HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, dwProcessId);
+    if (hProcess) {
+      DWORD dwExitCode;
+      // Wait until the process is no longer available
+      while (GetExitCodeProcess(hProcess, &dwExitCode) == STILL_ACTIVE) {
+        // TODO: Timeout
+        Sleep(1000);
       }
-    } while (hProcess != NULL);
+      CloseHandle(hProcess);
+    }
   }
-  // Wait a second
-  Sleep(1000);
   
   // Take a backup of the old file (App.exe -> App.exe.bak)
   WCHAR backup[MAX_PATH] = {'\0'};
   wcscpy_s(backup, MAX_PATH, argv[1]);
   wcscat_s(backup, MAX_PATH, L".bak");
-  while (!MoveFileW(argv[1], backup)) Sleep(1000);
+  DWORD dwFlags = MOVEFILE_REPLACE_EXISTING | MOVEFILE_WRITE_THROUGH;
+  MoveFileExW(argv[1], backup, dwFlags);
   
   // Rename the new file (App.exe.new -> App.exe)
-  if (MoveFileW(argv[2], argv[1])) {
-    Sleep(1000);
-    // Execute the new file
-    ShellExecuteW(NULL, L"open", argv[1], NULL, NULL, SW_SHOW);
+  if (MoveFileExW(argv[2], argv[1], dwFlags)) {
     // Delete the backup (App.exe.bak)
     DeleteFileW(backup);
+    // Execute the new file
+    ShellExecuteW(nullptr, L"open", argv[1], nullptr, nullptr, SW_SHOW);
   } else {
-    MessageBoxW(NULL, L"An error occured while updating.", NULL, MB_ICONERROR | MB_OK);
+    MessageBoxW(nullptr, L"An error occured while updating.", 
+                nullptr, MB_ICONERROR | MB_OK);
     // Revert to backup (App.exe.bak -> App.exe)
-    MoveFileW(backup, argv[1]);
+    MoveFileExW(backup, argv[1], dwFlags);
   }
 
   return 0;
