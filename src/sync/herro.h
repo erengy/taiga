81502e90eb2316d1fb3342ec1c68f0f7e6f61d36
Eren Okka <erenokka@gmail.com>
2013-11-27 02:56:31 +0200

    Add Herro as a service (not yet activated)

---
+++ src/sync/herro.h
@@ -0,0 +1,56 @@
+/*
+** Taiga
+** Copyright (C) 2010-2013, Eren Okka
+** 
+** This program is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+** 
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+** 
+** You should have received a copy of the GNU General Public License
+** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef TAIGA_SYNC_HERRO_H
+#define TAIGA_SYNC_HERRO_H
+
+#include "herro_types.h"
+#include "service.h"
+#include "base/types.h"
+
+namespace sync {
+namespace herro {
+
+// API documentation:
+// http://herro.co/api.html
+
+class Service : public sync::Service {
+public:
+  Service();
+  ~Service() {}
+
+  void BuildRequest(Request& request, HttpRequest& http_request);
+  void HandleResponse(Response& response, HttpResponse& http_response);
+  bool RequestNeedsAuthentication(RequestType request_type) const;
+
+private:
+  REQUEST_AND_RESPONSE(AddLibraryEntry);
+  REQUEST_AND_RESPONSE(AuthenticateUser);
+  REQUEST_AND_RESPONSE(DeleteLibraryEntry);
+  REQUEST_AND_RESPONSE(GetLibraryEntries);
+  REQUEST_AND_RESPONSE(GetMetadataById);
+  REQUEST_AND_RESPONSE(SearchTitle);
+  REQUEST_AND_RESPONSE(UpdateLibraryEntry);
+
+  bool RequestSucceeded(Response& response, const HttpResponse& http_response);
+};
+
+}  // namespace herro
+}  // namespace sync
+
+#endif  // TAIGA_SYNC_HERRO_H
