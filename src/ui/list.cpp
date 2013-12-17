09bd3713bd16e018e981c599ea5d077d6e42a9ff
Eren Okka <erenokka@gmail.com>
2013-12-17 13:43:17 +0200

    Merge changes from r263

--- src/ui/list.cpp
+++ src/ui/list.cpp
@@ -190,12 +190,24 @@ int SortListByProgress(const anime::Item& item1, const anime::Item& item2) {
       return -1;
     } else if (ratio1 < ratio2) {
       return 1;
+    } else {
+      if (total1 > total2) {
+        return -1;
+      } else if (total1 < total2) {
+        return 1;
+      }
     }
   } else {
     if (watched1 > watched2) {
       return -1;
     } else if (watched1 < watched2) {
       return 1;
+    } else {
+      if (total1 > total2) {
+        return -1;
+      } else if (total1 < total2) {
+        return 1;
+      }
     }
   }
 
