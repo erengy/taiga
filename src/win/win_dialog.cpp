e7620518afd31409871ba968fc7a805484d64f48
Eren Okka <erenokka@gmail.com>
2013-12-25 00:58:50 +0200

    Review win_dialog.*

--- src/win/win_dialog.cpp
+++ src/win/win_dialog.cpp
@@ -1,6 +1,6 @@
 /*
-** Taiga, a lightweight client for MyAnimeList
-** Copyright (C) 2010-2012, Eren Okka
+** Taiga
+** Copyright (C) 2010-2013, Eren Okka
 ** 
 ** This program is free software: you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
@@ -19,109 +19,126 @@
 #include "win_dialog.h"
 #include "win_taskbar.h"
 
+//#define TAIGA_WIN_DIALOG_REMEMBER_LAST_POSITION_AND_SIZE
+
 namespace win {
 
-// =============================================================================
+Dialog::Dialog()
+    : modal_(true), snap_gap_(0) {
+  size_last_.cx = 0;
+  size_last_.cy = 0;
 
-Dialog::Dialog() :
-  m_bModal(true), m_iSnapGap(0)
-{
-  m_SizeLast.cx = 0; m_SizeLast.cy = 0;
-  m_SizeMax.cx  = 0; m_SizeMax.cy  = 0;
-  m_SizeMin.cx  = 0; m_SizeMin.cy  = 0;
-  m_PosLast.x   = 0; m_PosLast.y   = 0;
+  size_max_.cx = 0;
+  size_max_.cy = 0;
+
+  size_min_.cx = 0;
+  size_min_.cy = 0;
+
+  pos_last_.x = 0;
+  pos_last_.y = 0;
 }
 
 Dialog::~Dialog() {
   EndDialog(0);
 }
 
-INT_PTR Dialog::Create(UINT uResourceID, HWND hParent, bool bModal) {
-  m_bModal = bModal;
-  if (bModal) {
-    INT_PTR nResult = ::DialogBoxParam(instance_, MAKEINTRESOURCE(uResourceID), 
-      hParent, DialogProcStatic, reinterpret_cast<LPARAM>(this));
-    window_ = NULL;
-    return nResult;
+INT_PTR Dialog::Create(UINT resource_id, HWND parent, bool modal) {
+  modal_ = modal;
+
+  if (modal) {
+    INT_PTR result = ::DialogBoxParam(
+        instance_, MAKEINTRESOURCE(resource_id), parent, DialogProcStatic,
+        reinterpret_cast<LPARAM>(this));
+    window_ = nullptr;
+    return result;
+
   } else {
-    window_ = ::CreateDialogParam(instance_, MAKEINTRESOURCE(uResourceID), 
-      hParent, DialogProcStatic, reinterpret_cast<LPARAM>(this));
+    window_ = ::CreateDialogParam(
+        instance_, MAKEINTRESOURCE(resource_id), parent, DialogProcStatic,
+        reinterpret_cast<LPARAM>(this));
     return reinterpret_cast<INT_PTR>(window_);
   }
 }
 
-void Dialog::EndDialog(INT_PTR nResult) {
-  // Remember last position and size
-  RECT rect; GetWindowRect(&rect);
-  m_PosLast.x = rect.left;
-  m_PosLast.y = rect.top;
-  m_SizeLast.cx = rect.right - rect.left;
-  m_SizeLast.cy = rect.bottom - rect.top;
+void Dialog::EndDialog(INT_PTR result) {
+#ifdef TAIGA_WIN_DIALOG_REMEMBER_LAST_POSITION_AND_SIZE
+  RECT rect;
+  GetWindowRect(&rect);
+  pos_last_.x = rect.left;
+  pos_last_.y = rect.top;
+  size_last_.cx = rect.right - rect.left;
+  size_last_.cy = rect.bottom - rect.top;
+#endif
 
-  // Destroy window
-  if (::IsWindow(window_)) {
-    if (m_bModal) {
-      ::EndDialog(window_, nResult);
+  if (IsWindow()) {
+    if (modal_) {
+      ::EndDialog(window_, result);
     } else {
       Destroy();
     }
   }
-  window_ = NULL;
+  window_ = nullptr;
 }
 
 void Dialog::SetSizeMax(LONG cx, LONG cy) {
-  m_SizeMax.cx = cx; m_SizeMax.cy = cy;
+  size_max_.cx = cx;
+  size_max_.cy = cy;
 }
 
 void Dialog::SetSizeMin(LONG cx, LONG cy) {
-  m_SizeMin.cx = cx; m_SizeMin.cy = cy;
+  size_min_.cx = cx;
+  size_min_.cy = cy;
 }
 
-void Dialog::SetSnapGap(int iSnapGap) {
-  m_iSnapGap = iSnapGap;
+void Dialog::SetSnapGap(int snap_gap) {
+  snap_gap_ = snap_gap;
 }
 
-// =============================================================================
+////////////////////////////////////////////////////////////////////////////////
 
-void Dialog::SetMinMaxInfo(LPMINMAXINFO lpMMI) {
-  if (m_SizeMax.cx > 0) lpMMI->ptMaxTrackSize.x = m_SizeMax.cx;
-  if (m_SizeMax.cy > 0) lpMMI->ptMaxTrackSize.y = m_SizeMax.cy;
-  if (m_SizeMin.cx > 0) lpMMI->ptMinTrackSize.x = m_SizeMin.cx;
-  if (m_SizeMin.cy > 0) lpMMI->ptMinTrackSize.y = m_SizeMin.cy;
+void Dialog::SetMinMaxInfo(LPMINMAXINFO mmi) {
+  if (size_max_.cx > 0)
+    mmi->ptMaxTrackSize.x = size_max_.cx;
+  if (size_max_.cy > 0)
+    mmi->ptMaxTrackSize.y = size_max_.cy;
+
+  if (size_min_.cx > 0)
+    mmi->ptMinTrackSize.x = size_min_.cx;
+  if (size_min_.cy > 0)
+    mmi->ptMinTrackSize.y = size_min_.cy;
 }
 
-void Dialog::SnapToEdges(LPWINDOWPOS lpWndPos) {
-  if (m_iSnapGap == 0) return;
+void Dialog::SnapToEdges(LPWINDOWPOS window_pos) {
+  if (!snap_gap_)
+    return;
 
-  HMONITOR hMonitor;
-  MONITORINFO mi;
-  RECT mon;
-
-  // Get monitor info
-  SystemParametersInfo(SPI_GETWORKAREA, NULL, &mon, NULL);
+  RECT rc_monitor = {0};
+  SystemParametersInfo(SPI_GETWORKAREA, 0, &rc_monitor, 0);
   if (GetSystemMetrics(SM_CMONITORS) > 1) {
-    hMonitor = MonitorFromWindow(window_, MONITOR_DEFAULTTONEAREST);
-    if (hMonitor) {
+    HMONITOR monitor = MonitorFromWindow(window_, MONITOR_DEFAULTTONEAREST);
+    if (monitor) {
+      MONITORINFO mi;
       mi.cbSize = sizeof(mi);
-      GetMonitorInfo(hMonitor, &mi);
-      mon = mi.rcWork;
+      GetMonitorInfo(monitor, &mi);
+      rc_monitor = mi.rcWork;
     }
   }
+
   // Snap X axis
-  if (abs(lpWndPos->x - mon.left) <= m_iSnapGap) {
-    lpWndPos->x = mon.left;
-  } else if (abs(lpWndPos->x + lpWndPos->cx - mon.right) <= m_iSnapGap) {
-    lpWndPos->x = mon.right - lpWndPos->cx;
+  if (abs(window_pos->x - rc_monitor.left) <= snap_gap_) {
+    window_pos->x = rc_monitor.left;
+  } else if (abs(window_pos->x + window_pos->cx - rc_monitor.right) <= snap_gap_) {
+    window_pos->x = rc_monitor.right - window_pos->cx;
   }
   // Snap Y axis
-  if (abs(lpWndPos->y - mon.top) <= m_iSnapGap) {
-    lpWndPos->y = mon.top;
-  } else if (abs(lpWndPos->y + lpWndPos->cy - mon.bottom) <= m_iSnapGap) {
-    lpWndPos->y = mon.bottom - lpWndPos->cy;
+  if (abs(window_pos->y - rc_monitor.top) <= snap_gap_) {
+    window_pos->y = rc_monitor.top;
+  } else if (abs(window_pos->y + window_pos->cy - rc_monitor.bottom) <= snap_gap_) {
+    window_pos->y = rc_monitor.bottom - window_pos->cy;
   }
 }
 
-// =============================================================================
+////////////////////////////////////////////////////////////////////////////////
 
 void Dialog::OnCancel() {
   EndDialog(IDCANCEL);
@@ -140,98 +157,103 @@ void Dialog::OnOK() {
 }
 
 // Superclassing
-void Dialog::RegisterDlgClass(LPCWSTR lpszClassName) {
+void Dialog::RegisterDlgClass(LPCWSTR class_name) {
   WNDCLASS wc;
-  ::GetClassInfo(instance_, WC_DIALOG, &wc); // WC_DIALOG is #32770
-  wc.lpszClassName = lpszClassName;
+  ::GetClassInfo(instance_, WC_DIALOG, &wc);  // WC_DIALOG is #32770
+  wc.lpszClassName = class_name;
   ::RegisterClass(&wc);
 }
 
-// =============================================================================
+////////////////////////////////////////////////////////////////////////////////
 
-BOOL Dialog::AddComboString(int nIDDlgItem, LPCWSTR lpString) {
-  return ::SendDlgItemMessage(window_, nIDDlgItem, CB_ADDSTRING, 0, reinterpret_cast<LPARAM>(lpString));
+BOOL Dialog::AddComboString(int id_combo, LPCWSTR text) {
+  return SendDlgItemMessage(id_combo, CB_ADDSTRING, 0,
+                            reinterpret_cast<LPARAM>(text));
 }
 
-BOOL Dialog::CheckDlgButton(int nIDButton, UINT uCheck) {
-  return ::CheckDlgButton(window_, nIDButton, uCheck);
+BOOL Dialog::CheckDlgButton(int id_button, UINT check) {
+  return ::CheckDlgButton(window_, id_button, check);
 }
 
-BOOL Dialog::CheckRadioButton(int nIDFirstButton, int nIDLastButton, int nIDCheckButton) {
-  return ::CheckRadioButton(window_, nIDFirstButton, nIDLastButton, nIDCheckButton);
+BOOL Dialog::CheckRadioButton(int id_first_button, int id_last_button,
+                              int id_check_button) {
+  return ::CheckRadioButton(window_, id_first_button, id_last_button,
+                            id_check_button);
 }
 
-BOOL Dialog::EnableDlgItem(int nIDDlgItem, BOOL bEnable) {
-  return ::EnableWindow(::GetDlgItem(window_, nIDDlgItem), bEnable);
+BOOL Dialog::EnableDlgItem(int id_item, BOOL enable) {
+  return ::EnableWindow(GetDlgItem(id_item), enable);
 }
 
-INT Dialog::GetCheckedRadioButton(int nIDFirstButton, int nIDLastButton) {
-  for (int i = 0; i <= nIDLastButton - nIDFirstButton; i++) {
-    if (::IsDlgButtonChecked(window_, nIDFirstButton + i)) {
+INT Dialog::GetCheckedRadioButton(int id_first_button, int id_last_button) {
+  for (int i = 0; i <= id_last_button - id_first_button; i++)
+    if (IsDlgButtonChecked(id_first_button + i))
       return i;
-    }
-  }
+
   return 0;
 }
 
-INT Dialog::GetComboSelection(int nIDDlgItem) {
-  return ::SendDlgItemMessage(window_, nIDDlgItem, CB_GETCURSEL, 0, 0);
+INT Dialog::GetComboSelection(int id_item) {
+  return SendDlgItemMessage(id_item, CB_GETCURSEL, 0, 0);
 }
 
-HWND Dialog::GetDlgItem(int nIDDlgItem) {
-  return ::GetDlgItem(window_, nIDDlgItem);
+HWND Dialog::GetDlgItem(int id_item) {
+  return ::GetDlgItem(window_, id_item);
 }
 
-UINT Dialog::GetDlgItemInt(int nIDDlgItem) {
-  return ::GetDlgItemInt(window_, nIDDlgItem, NULL, TRUE);
+UINT Dialog::GetDlgItemInt(int id_item) {
+  return ::GetDlgItemInt(window_, id_item, nullptr, TRUE);
 }
 
-void Dialog::GetDlgItemText(int nIDDlgItem, LPWSTR lpString, int cchMax) {
-  ::GetDlgItemText(window_, nIDDlgItem, lpString, cchMax);
+void Dialog::GetDlgItemText(int id_item, LPWSTR output, int max_length) {
+  ::GetDlgItemText(window_, id_item, output, max_length);
 }
 
-void Dialog::GetDlgItemText(int nIDDlgItem, wstring& str) {
-  int len = ::GetWindowTextLength(::GetDlgItem(window_, nIDDlgItem)) + 1;
+void Dialog::GetDlgItemText(int id_item, wstring& output) {
+  int len = ::GetWindowTextLength(GetDlgItem(id_item)) + 1;
   vector<wchar_t> buffer(len);
-  ::GetDlgItemText(window_, nIDDlgItem, &buffer[0], len);
-  str.assign(&buffer[0]);
+  ::GetDlgItemText(window_, id_item, &buffer[0], len);
+  output.assign(&buffer[0]);
 }
 
-wstring Dialog::GetDlgItemText(int nIDDlgItem) {
-  int len = ::GetWindowTextLength(::GetDlgItem(window_, nIDDlgItem)) + 1;
+wstring Dialog::GetDlgItemText(int id_item) {
+  int len = ::GetWindowTextLength(GetDlgItem(id_item)) + 1;
   vector<wchar_t> buffer(len);
-  ::GetDlgItemText(window_, nIDDlgItem, &buffer[0], len);
+  ::GetDlgItemText(window_, id_item, &buffer[0], len);
   return wstring(&buffer[0]);
 }
 
-BOOL Dialog::HideDlgItem(int nIDDlgItem) {
-  return ::ShowWindow(::GetDlgItem(window_, nIDDlgItem), SW_HIDE);
+BOOL Dialog::HideDlgItem(int id_item) {
+  return ::ShowWindow(GetDlgItem(id_item), SW_HIDE);
 }
 
-BOOL Dialog::IsDlgButtonChecked(int nIDButton) {
-  return ::IsDlgButtonChecked(window_, nIDButton);
+BOOL Dialog::IsDlgButtonChecked(int id_button) {
+  return ::IsDlgButtonChecked(window_, id_button);
 }
 
-BOOL Dialog::SendDlgItemMessage(int nIDDlgItem, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-  return ::SendDlgItemMessage(window_, nIDDlgItem, uMsg, wParam, lParam);
+BOOL Dialog::SendDlgItemMessage(int id_item, UINT uMsg,
+                                WPARAM wParam, LPARAM lParam) {
+  return ::SendDlgItemMessage(window_, id_item, uMsg, wParam, lParam);
 }
 
-BOOL Dialog::SetComboSelection(int nIDDlgItem, int iIndex) {
-  return ::SendDlgItemMessage(window_, nIDDlgItem, CB_SETCURSEL, iIndex, 0);
+BOOL Dialog::SetComboSelection(int id_item, int index) {
+  return SendDlgItemMessage(id_item, CB_SETCURSEL, index, 0);
 }
 
-BOOL Dialog::SetDlgItemText(int nIDDlgItem, LPCWSTR lpString) {
-  return ::SetDlgItemText(window_, nIDDlgItem, lpString);
+BOOL Dialog::SetDlgItemText(int id_item, LPCWSTR text) {
+  return ::SetDlgItemText(window_, id_item, text);
 }
 
-BOOL Dialog::ShowDlgItem(int nIDDlgItem, int nCmdShow) {
-  return ::ShowWindow(::GetDlgItem(window_, nIDDlgItem), nCmdShow);
+BOOL Dialog::ShowDlgItem(int id_item, int cmd_show) {
+  return ::ShowWindow(GetDlgItem(id_item), cmd_show);
 }
 
-// =============================================================================
+////////////////////////////////////////////////////////////////////////////////
 
-INT_PTR CALLBACK Dialog::DialogProcStatic(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+INT_PTR CALLBACK Dialog::DialogProcStatic(HWND hwnd, UINT uMsg,
+                                          WPARAM wParam, LPARAM lParam) {
   Dialog* window = reinterpret_cast<Dialog*>(WindowMap.GetWindow(hwnd));
+
   if (!window && uMsg == WM_INITDIALOG) {
     window = reinterpret_cast<Dialog*>(lParam);
     if (window) {
@@ -239,6 +261,7 @@ INT_PTR CALLBACK Dialog::DialogProcStatic(HWND hwnd, UINT uMsg, WPARAM wParam, L
       WindowMap.Add(hwnd, window);
     }
   }
+
   if (window) {
     return window->DialogProc(hwnd, uMsg, wParam, lParam);
   } else {
@@ -250,7 +273,8 @@ INT_PTR Dialog::DialogProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
   return DialogProcDefault(hwnd, uMsg, wParam, lParam);
 }
 
-INT_PTR Dialog::DialogProcDefault(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+INT_PTR Dialog::DialogProcDefault(HWND hwnd, UINT uMsg,
+                                  WPARAM wParam, LPARAM lParam) {
   switch (uMsg) {
     case WM_CLOSE: {
       return OnClose();
@@ -273,12 +297,12 @@ INT_PTR Dialog::DialogProcDefault(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lP
       break;
     }
     case WM_INITDIALOG: {
-      /*if (m_PosLast.x && m_PosLast.y) {
-        SetPosition(NULL, m_PosLast.x, m_PosLast.y, 0, 0, SWP_NOSIZE);
-      }
-      if (m_SizeLast.cx && m_SizeLast.cy) {
-        SetPosition(NULL, 0, 0, m_SizeLast.cx, m_SizeLast.cy, SWP_NOMOVE);
-      }*/
+#ifdef TAIGA_WIN_DIALOG_REMEMBER_LAST_POSITION_AND_SIZE
+      if (pos_last_.x && pos_last_.y)
+        SetPosition(nullptr, pos_last_.x, pos_last_.y, 0, 0, SWP_NOSIZE);
+      if (size_last_.cx && size_last_.cy)
+        SetPosition(nullptr, 0, 0, size_last_.cx, size_last_.cy, SWP_NOMOVE);
+#endif
       return OnInitDialog();
     }
     case WM_DROPFILES: {
@@ -292,9 +316,9 @@ INT_PTR Dialog::DialogProcDefault(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lP
       break;
     }
     case WM_GETMINMAXINFO: {
-      LPMINMAXINFO lpMMI = reinterpret_cast<LPMINMAXINFO>(lParam);
-      SetMinMaxInfo(lpMMI);
-      OnGetMinMaxInfo(lpMMI);
+      LPMINMAXINFO mmi = reinterpret_cast<LPMINMAXINFO>(lParam);
+      SetMinMaxInfo(mmi);
+      OnGetMinMaxInfo(mmi);
       break;
     }
     case WM_LBUTTONDOWN:
@@ -309,9 +333,9 @@ INT_PTR Dialog::DialogProcDefault(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lP
     case WM_MOUSELEAVE:
     case WM_MOUSEMOVE:
     case WM_MOUSEWHEEL: {
-      LRESULT lResult = OnMouseEvent(uMsg, wParam, lParam);
-      if (lResult != -1) {
-        ::SetWindowLongPtrW(hwnd, DWL_MSGRESULT, lResult);
+      LRESULT result = OnMouseEvent(uMsg, wParam, lParam);
+      if (result != -1) {
+        ::SetWindowLongPtrW(hwnd, DWL_MSGRESULT, result);
         return TRUE;
       }
       break;
@@ -322,22 +346,22 @@ INT_PTR Dialog::DialogProcDefault(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lP
       break;
     }
     case WM_NOTIFY: {
-      LRESULT lResult = OnNotify(wParam, reinterpret_cast<LPNMHDR>(lParam));
-      if (lResult) {
-        ::SetWindowLongPtr(hwnd, DWL_MSGRESULT, lResult);
+      LRESULT result = OnNotify(wParam, reinterpret_cast<LPNMHDR>(lParam));
+      if (result) {
+        ::SetWindowLongPtr(hwnd, DWL_MSGRESULT, result);
         return TRUE;
       }
       break;
     }
     case WM_PAINT: {
-      if (::GetUpdateRect(hwnd, NULL, FALSE)) {
+      if (::GetUpdateRect(hwnd, nullptr, FALSE)) {
         PAINTSTRUCT ps;
         HDC hdc = ::BeginPaint(hwnd, &ps);
         OnPaint(hdc, &ps);
         ::EndPaint(hwnd, &ps);
       } else {
         HDC hdc = ::GetDC(hwnd);
-        OnPaint(hdc, NULL);
+        OnPaint(hdc, nullptr);
         ::ReleaseDC(hwnd, hdc);
       }
       break;
@@ -352,17 +376,17 @@ INT_PTR Dialog::DialogProcDefault(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lP
       break;
     }
     case WM_WINDOWPOSCHANGING: {
-      LPWINDOWPOS lpWndPos = reinterpret_cast<LPWINDOWPOS>(lParam);
-      SnapToEdges(lpWndPos);
-      OnWindowPosChanging(lpWndPos);
+      LPWINDOWPOS window_pos = reinterpret_cast<LPWINDOWPOS>(lParam);
+      SnapToEdges(window_pos);
+      OnWindowPosChanging(window_pos);
       break;
     }
     default: {
-      if (uMsg == WM_TASKBARCREATED || 
-          uMsg == WM_TASKBARBUTTONCREATED || 
+      if (uMsg == WM_TASKBARCREATED ||
+          uMsg == WM_TASKBARBUTTONCREATED ||
           uMsg == WM_TASKBARCALLBACK) {
-            OnTaskbarCallback(uMsg, lParam);
-            return FALSE;
+        OnTaskbarCallback(uMsg, lParam);
+        return FALSE;
       }
       break;
     }
--- src/win/win_dialog.h
+++ src/win/win_dialog.h
@@ -1,6 +1,6 @@
 /*
-** Taiga, a lightweight client for MyAnimeList
-** Copyright (C) 2010-2012, Eren Okka
+** Taiga
+** Copyright (C) 2010-2013, Eren Okka
 ** 
 ** This program is free software: you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
@@ -16,51 +16,49 @@
 ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef WIN_DIALOG_H
-#define WIN_DIALOG_H
+#ifndef TAIGA_WIN_DIALOG_H
+#define TAIGA_WIN_DIALOG_H
 
 #include "win_main.h"
 #include "win_window.h"
 
 namespace win {
 
-// =============================================================================
-
 class Dialog : public Window {
 public:
   Dialog();
   virtual ~Dialog();
-  
-  virtual INT_PTR Create(UINT uResourceID, HWND hParent = NULL, bool bModal = true);
-  virtual void EndDialog(INT_PTR nResult);
+
+  virtual INT_PTR Create(UINT resource_id, HWND parent = nullptr, bool modal = true);
+  virtual void EndDialog(INT_PTR result);
   virtual void SetSizeMax(LONG cx, LONG cy);
   virtual void SetSizeMin(LONG cx, LONG cy);
-  virtual void SetSnapGap(int iSnapGap);
+  virtual void SetSnapGap(int snap_gap);
 
-  virtual BOOL AddComboString(int nIDDlgItem, LPCWSTR lpString);
-  virtual BOOL CheckDlgButton(int nIDButton, UINT uCheck);
-  virtual BOOL CheckRadioButton(int nIDFirstButton, int nIDLastButton, int nIDCheckButton);
-  virtual BOOL EnableDlgItem(int nIDDlgItem, BOOL bEnable);
-  virtual INT  GetCheckedRadioButton(int nIDFirstButton, int nIDLastButton);
-  virtual INT  GetComboSelection(int nIDDlgItem);
-  virtual HWND GetDlgItem(int nIDDlgItem);
-  virtual UINT GetDlgItemInt(int nIDDlgItem);
-  virtual void GetDlgItemText(int nIDDlgItem, LPWSTR lpString, int cchMax = MAX_PATH);
-  virtual void GetDlgItemText(int nIDDlgItem, wstring& str);
-  virtual wstring GetDlgItemText(int nIDDlgItem);
-  virtual BOOL HideDlgItem(int nIDDlgItem);
-  virtual BOOL IsDlgButtonChecked(int nIDButton);
-  virtual BOOL SendDlgItemMessage(int nIDDlgItem, UINT uMsg, WPARAM wParam, LPARAM lParam);
-  virtual BOOL SetComboSelection(int nIDDlgItem, int iIndex);
-  virtual BOOL SetDlgItemText(int nIDDlgItem, LPCWSTR lpString);
-  virtual BOOL ShowDlgItem(int nIDDlgItem, int nCmdShow = SW_SHOWNORMAL);
+  virtual BOOL AddComboString(int id_combo, LPCWSTR text);
+  virtual BOOL CheckDlgButton(int id_button, UINT check);
+  virtual BOOL CheckRadioButton(int id_first_button, int id_last_button, int id_check_button);
+  virtual BOOL EnableDlgItem(int id_item, BOOL enable);
+  virtual INT  GetCheckedRadioButton(int id_first_button, int id_last_button);
+  virtual INT  GetComboSelection(int id_item);
+  virtual HWND GetDlgItem(int id_item);
+  virtual UINT GetDlgItemInt(int id_item);
+  virtual void GetDlgItemText(int id_item, LPWSTR output, int max_length = MAX_PATH);
+  virtual void GetDlgItemText(int id_item, wstring& output);
+  virtual wstring GetDlgItemText(int id_item);
+  virtual BOOL HideDlgItem(int id_item);
+  virtual BOOL IsDlgButtonChecked(int id_button);
+  virtual BOOL SendDlgItemMessage(int id_item, UINT uMsg, WPARAM wParam, LPARAM lParam);
+  virtual BOOL SetComboSelection(int id_item, int index);
+  virtual BOOL SetDlgItemText(int id_item, LPCWSTR text);
+  virtual BOOL ShowDlgItem(int id_item, int cmd_show = SW_SHOWNORMAL);
 
 protected:
   virtual void OnCancel();
   virtual BOOL OnClose();
   virtual BOOL OnInitDialog();
   virtual void OnOK();
-  virtual void RegisterDlgClass(LPCWSTR lpszClassName);
+  virtual void RegisterDlgClass(LPCWSTR class_name);
 
   virtual INT_PTR DialogProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
   virtual INT_PTR DialogProcDefault(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
@@ -68,15 +66,15 @@ protected:
 private:
   static INT_PTR CALLBACK DialogProcStatic(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-  void SetMinMaxInfo(LPMINMAXINFO lpMMI);
-  void SnapToEdges(LPWINDOWPOS lpWndPos);
+  void SetMinMaxInfo(LPMINMAXINFO mmi);
+  void SnapToEdges(LPWINDOWPOS window_pos);
 
-  bool  m_bModal;
-  int   m_iSnapGap;
-  SIZE  m_SizeLast, m_SizeMax, m_SizeMin;
-  POINT m_PosLast;
+  bool  modal_;
+  int   snap_gap_;
+  SIZE  size_last_, size_max_, size_min_;
+  POINT pos_last_;
 };
 
 }  // namespace win
 
-#endif // WIN_DIALOG_H
+#endif  // TAIGA_WIN_DIALOG_H
