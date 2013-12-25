f63354bae4e8bc1a0a86cf6116bcb5a251ecca71
Eren Okka <erenokka@gmail.com>
2013-12-25 19:04:57 +0200

    Add JsonCpp library

--- src/ui/dlg/dlg_about.cpp
+++ src/ui/dlg/dlg_about.cpp
@@ -110,6 +110,7 @@ BOOL AboutDialog::OnInitDialog() {
     L"saka, Diablofan, slevir, LordGravewish\\line\\line "
     L"\\b Third party stuff that is used by Taiga:\\b0\\line "
     L"- Fugue Icons 3.4.5, Copyright (c) 2012, Yusuke Kamiyamane\\line "
+    L"- JsonCpp 0.6.0, Copyright (c) 2007-2010, Baptiste Lepilleur\\line "
     L"- OAuth class is based on codebrook-twitter-oauth example code, Copyright (c) 2010, Brook Miles\\line "
     L"- pugixml parser version 1.2, Copyright (c) 2006-2012, Arseny Kapoulkine\\line "
     L"- zlib version 1.2.5, Copyright (c) 1995-2010, Jean-loup Gailly and Mark Adler\\line\\line "
