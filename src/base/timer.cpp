147e0f668fe4944463e4d4f127589438aee4ceed
Eren Okka <erenokka@gmail.com>
2014-01-25 23:37:20 +0200

    Add base::timer.*

---
+++ src/base/timer.cpp
@@ -0,0 +1,126 @@
+/*
+** Taiga
+** Copyright (C) 2010-2014, Eren Okka
+** 
+** This program is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+** 
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+** 
+** You should have received a copy of the GNU General Public License
+** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "timer.h"
+
+namespace base {
+
+Timer::Timer(unsigned int id, int interval, bool repeat)
+    : enabled_(true),
+      id_(id),
+      interval_(interval),
+      repeat_(repeat),
+      ticks_(0) {
+}
+
+bool Timer::enabled() const {
+  return enabled_;
+}
+
+unsigned int Timer::id() const {
+  return id_;
+}
+
+int Timer::interval() const {
+  return interval_;
+}
+
+bool Timer::repeat() const {
+  return repeat_;
+}
+
+int Timer::ticks() const {
+  return ticks_;
+}
+
+void Timer::set_enabled(bool enabled) {
+  enabled_ = enabled;
+}
+
+void Timer::set_id(unsigned int id) {
+  id_ = id;
+}
+
+void Timer::set_interval(int interval) {
+  int interval_difference = interval - interval_;
+  interval_ = interval;
+  ticks_ += interval_difference;
+
+  if (ticks_ < 1)
+    ticks_ = 1;  // So that it reaches 0 on next tick
+}
+
+void Timer::set_repeat(bool repeat) {
+  repeat_ = repeat;
+}
+
+void Timer::set_ticks(int ticks) {
+  ticks_ = ticks;
+
+  if (ticks_ > interval_)
+    ticks_ = interval_;
+}
+
+void Timer::Reset() {
+  ticks_ = interval_;
+}
+
+void Timer::Tick() {
+  if (!enabled_)
+    return;
+
+  if (interval_ > 0 && ticks_ > 0)
+    ticks_ -= 1;
+
+  if (ticks_ == 0)
+    OnTimeout();
+
+  if (ticks_ == 0 && repeat_)
+    Reset();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+TimerManager::TimerManager()
+    : hwnd_(nullptr), id_(0) {
+}
+
+TimerManager::~TimerManager() {
+  if (id_ != 0)
+    ::KillTimer(hwnd_, id_);
+}
+
+Timer* TimerManager::timer(unsigned int id) {
+  if (timers_.find(id) != timers_.end())
+    return timers_[id];
+
+  return nullptr;
+}
+
+bool TimerManager::Initialize(HWND hwnd, TIMERPROC proc) {
+  hwnd_ = hwnd;
+  id_ = ::SetTimer(hwnd, 0, 1000 /*milliseconds*/, proc);
+
+  return id_ != 0;
+}
+
+void TimerManager::InsertTimer(const Timer* timer) {
+  timers_.insert(std::make_pair(timer->id(), const_cast<Timer*>(timer)));
+}
+
+}  // namespace base
---
+++ src/base/timer.h
@@ -0,0 +1,82 @@
+/*
+** Taiga
+** Copyright (C) 2010-2014, Eren Okka
+** 
+** This program is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+** 
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+** 
+** You should have received a copy of the GNU General Public License
+** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef TAIGA_BASE_TIMER_H
+#define TAIGA_BASE_TIMER_H
+
+#include <map>
+#include <windows.h>
+
+namespace base {
+
+class Timer {
+public:
+  Timer(unsigned int id, int interval, bool repeat);
+  virtual ~Timer() {}
+
+  bool enabled() const;
+  unsigned int id() const;
+  int interval() const;
+  bool repeat() const;
+  int ticks() const;
+
+  void set_enabled(bool enabled);
+  void set_id(unsigned int id);
+  void set_interval(int interval);
+  void set_repeat(bool repeat);
+  void set_ticks(int ticks);
+
+  // Resets ticks to interval
+  void Reset();
+
+  // Counts down from the interval
+  void Tick();
+
+protected:
+  // What to do when timer reaches zero
+  virtual void OnTimeout() = 0;
+
+private:
+  bool enabled_;
+  unsigned int id_;
+  int interval_;
+  bool repeat_;
+  int ticks_;
+};
+
+class TimerManager {
+public:
+  TimerManager();
+  virtual ~TimerManager();
+
+  Timer* timer(unsigned int id);
+
+  bool Initialize(HWND hwnd, TIMERPROC proc);
+  void InsertTimer(const Timer* timer);
+
+  virtual void OnTick() = 0;
+
+private:
+  HWND hwnd_;
+  UINT_PTR id_;
+  std::map<unsigned int, Timer*> timers_;
+};
+
+}  // namespace base
+
+#endif  // TAIGA_BASE_TIMER_H
