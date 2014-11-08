erengy/taiga

commit 13c3435a481eb2b2abc440c8534096ac31ea1a51

Add progress colors for on-hold and plan-to-watch to themes commit: f451539fe204e32be5cc52deb56765709ac13a4e git-svn-id: http://taiga.googlecode.com/svn/trunk@272 77add795-4bdf-f0f6-a574-f4d068491005

src/ui/dlg/dlg_anime_list.cpp
@@ -761,6 +761,29 @@ void AnimeListDialog::ListView::DrawProgressBar(HDC hdc, RECT* rc, int index,
     } else {
       ui::Theme.DrawListProgress(dc.Get(), &rcWatched, ui::kListProgressCompleted); // Completed / On hold / Plan to watch
     }
+    // Draw progress
+    if (anime_item.GetMyRewatching()) {
+      ui::Theme.DrawListProgress(dc.Get(), &rcWatched, ui::kListProgressWatching);
+    } else {
+      switch (anime_item.GetMyStatus()) {
+        default:
+        case anime::kWatching:
+          ui::Theme.DrawListProgress(dc.Get(), &rcWatched, ui::kListProgressWatching);
+          break;
+        case anime::kCompleted:
+          ui::Theme.DrawListProgress(dc.Get(), &rcWatched, ui::kListProgressCompleted);
+          break;
+        case anime::kOnHold:
+          ui::Theme.DrawListProgress(dc.Get(), &rcWatched, ui::kListProgressOnHold);
+          break;
+        case anime::kDropped:
+          ui::Theme.DrawListProgress(dc.Get(), &rcWatched, ui::kListProgressDropped);
+          break;
+        case anime::kPlanToWatch:
+          ui::Theme.DrawListProgress(dc.Get(), &rcWatched, ui::kListProgressPlanToWatch);
+          break;
+      }
+    }
   }
 
   // Draw episode availability

data/theme/Default/Theme.xml
@@ -55,15 +55,17 @@
 	<!-- List -->
 	<list>
 		<progress>
-			<border     type="solid" value_1="E0E0E0"/>
-			<background type="solid" value_1="FFFFFF"/>
-			<button     type="solid" value_1="646464"/>
-			<separator  type="solid" value_1="E0E0E0"/>
-			<watching   type="solid" value_1="60C653"/>
-			<completed  type="solid" value_1="60C653"/>
-			<dropped    type="solid" value_1="60C653"/>
-			<available  type="solid" value_1="CBCBCB"/>
-			<aired      type="solid" value_1="EBEBEB"/>
+			<border      type="solid" value_1="E0E0E0"/>
+			<background  type="solid" value_1="FFFFFF"/>
+			<button      type="solid" value_1="646464"/>
+			<separator   type="solid" value_1="E0E0E0"/>
+			<watching    type="solid" value_1="60C653"/>
+			<completed   type="solid" value_1="60C653"/>
+			<onhold      type="solid" value_1="60C653"/>
+			<dropped     type="solid" value_1="60C653"/>
+			<plantowatch type="solid" value_1="60C653"/>
+			<available   type="solid" value_1="CBCBCB"/>
+			<aired       type="solid" value_1="EBEBEB"/>
 		</progress>
 	</list>
 </theme>

src/ui/theme.cpp
@@ -73,6 +73,8 @@ bool ThemeManager::Load() {
   READ_PROGRESS_DATA(kListProgressButton, L"button");
   READ_PROGRESS_DATA(kListProgressCompleted, L"completed");
   READ_PROGRESS_DATA(kListProgressDropped, L"dropped");
+  READ_PROGRESS_DATA(kListProgressOnHold, L"onhold");
+  READ_PROGRESS_DATA(kListProgressPlanToWatch, L"plantowatch");
   READ_PROGRESS_DATA(kListProgressSeparator, L"separator");
   READ_PROGRESS_DATA(kListProgressWatching, L"watching");
   #undef READ_PROGRESS_DATA

src/ui/theme.h
@@ -86,6 +86,8 @@ enum ListProgressType {
   kListProgressButton,
   kListProgressCompleted,
   kListProgressDropped,
+  kListProgressOnHold,
+  kListProgressPlanToWatch,
   kListProgressSeparator,
   kListProgressWatching
 };

commit b5c720f16e1db6045e027c8326e45f5722c81358

Fix bug where progress bars are drawn twice
Seems to be introduced in 13c3435a481eb2b2abc440c8534096ac31ea1a51

src/ui/dlg/dlg_anime_list.cpp
@@ -788,38 +788,30 @@ void AnimeListDialog::ListView::DrawProgressBar(HDC hdc, RECT* rc, int index,
     }
 
     // Draw progress
-    if (anime_item.GetMyStatus() == anime::kWatching || anime_item.GetMyRewatching()) {
-      ui::Theme.DrawListProgress(dc.Get(), &rcWatched, ui::kListProgressWatching);  // Watching
-    } else if (anime_item.GetMyStatus() == anime::kCompleted) {
-      ui::Theme.DrawListProgress(dc.Get(), &rcWatched, ui::kListProgressCompleted); // Completed
-    } else if (anime_item.GetMyStatus() == anime::kDropped) {
-      ui::Theme.DrawListProgress(dc.Get(), &rcWatched, ui::kListProgressDropped);   // Dropped
-    } else {
-      ui::Theme.DrawListProgress(dc.Get(), &rcWatched, ui::kListProgressCompleted); // Completed / On hold / Plan to watch
-    }
-    // Draw progress
+    auto list_progress_type = ui::kListProgressWatching;
     if (anime_item.GetMyRewatching()) {
-      ui::Theme.DrawListProgress(dc.Get(), &rcWatched, ui::kListProgressWatching);
+      list_progress_type = ui::kListProgressWatching;
     } else {
       switch (anime_item.GetMyStatus()) {
         default:
         case anime::kWatching:
-          ui::Theme.DrawListProgress(dc.Get(), &rcWatched, ui::kListProgressWatching);
+          list_progress_type = ui::kListProgressWatching;
           break;
         case anime::kCompleted:
-          ui::Theme.DrawListProgress(dc.Get(), &rcWatched, ui::kListProgressCompleted);
+          list_progress_type = ui::kListProgressCompleted;
           break;
         case anime::kOnHold:
-          ui::Theme.DrawListProgress(dc.Get(), &rcWatched, ui::kListProgressOnHold);
+          list_progress_type = ui::kListProgressOnHold;
           break;
         case anime::kDropped:
-          ui::Theme.DrawListProgress(dc.Get(), &rcWatched, ui::kListProgressDropped);
+          list_progress_type = ui::kListProgressDropped;
           break;
         case anime::kPlanToWatch:
-          ui::Theme.DrawListProgress(dc.Get(), &rcWatched, ui::kListProgressPlanToWatch);
+          list_progress_type = ui::kListProgressPlanToWatch;
           break;
       }
     }
+    ui::Theme.DrawListProgress(dc.Get(), &rcWatched, list_progress_type);
   }
 
   // Draw episode availability