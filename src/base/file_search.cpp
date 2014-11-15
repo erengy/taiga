erengy/taiga

commit fb6482f0827d084c335760e2ef77dc6c9a325b9e

Ignore hidden files and system files
Fixes issue #20

src/base/file_search.cpp
@@ -41,30 +41,30 @@ bool FileSearchHelper::Search(const std::wstring& root) {
 
   do {
     if (handle == INVALID_HANDLE_VALUE) {
-      LOG(LevelError, Logger::FormatError(GetLastError()));
-      LOG(LevelError, L"Path: " + path);
+      LOG(LevelError, Logger::FormatError(GetLastError()) + L"\nPath: " + path);
       SetLastError(ERROR_SUCCESS);
-      return false;
+      continue;
     }
+
+    if (IsSystemFile(find_data) || IsHiddenFile(find_data))
+      continue;
 
     // Directory
     if (IsDirectory(find_data)) {
-      if (IsValidDirectory(find_data)) {
-        if (!skip_directories_)
-          result = OnDirectory(root, find_data.cFileName);
-        if (!skip_subdirectories_ && !result)
-          result = Search(AddTrailingSlash(root) + find_data.cFileName);
-      }
+      if (skip_directories_)
+        continue;
+      if (!IsValidDirectory(find_data))
+        continue;
+      result = OnDirectory(root, find_data.cFileName);
+      if (!result)
+        result = Search(AddTrailingSlash(root) + find_data.cFileName);
 
     // File
-    } else if (!skip_files_) {
-      if (find_data.nFileSizeLow < minimum_file_size_) {
-        LOG(LevelDebug,
-            L"File is ignored because its size does not meet the threshold.");
-        LOG(LevelDebug,
-            L"Path: " + AddTrailingSlash(root) + find_data.cFileName);
+    } else {
+      if (skip_files_)
         continue;
-      }
+      if (find_data.nFileSizeLow < minimum_file_size_)
+        continue;
       result = OnFile(root, find_data.cFileName);
     }
 

src/base/file.cpp
@@ -176,6 +176,14 @@ bool IsDirectory(const WIN32_FIND_DATA& find_data) {
   return (find_data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) != 0;
 }
 
+bool IsHiddenFile(const WIN32_FIND_DATA& find_data) {
+  return (find_data.dwFileAttributes & FILE_ATTRIBUTE_HIDDEN) != 0;
+}
+
+bool IsSystemFile(const WIN32_FIND_DATA& find_data) {
+  return (find_data.dwFileAttributes & FILE_ATTRIBUTE_SYSTEM) != 0;
+}
+
 bool IsValidDirectory(const WIN32_FIND_DATA& find_data) {
   return wcscmp(find_data.cFileName, L".") != 0 &&
          wcscmp(find_data.cFileName, L"..") != 0;

src/base/file.h
@@ -38,6 +38,8 @@ int DeleteFolder(std::wstring path);
 
 std::wstring GetExtendedLengthPath(const std::wstring& path);
 bool IsDirectory(const WIN32_FIND_DATA& find_data);
+bool IsHiddenFile(const WIN32_FIND_DATA& find_data);
+bool IsSystemFile(const WIN32_FIND_DATA& find_data);
 bool IsValidDirectory(const WIN32_FIND_DATA& find_data);
 
 bool FileExists(const std::wstring& file);

commit 58a21952a6470225d5ae6d06ad659dbd63730069

Fix bug where subdirectories are ignored
Introduced in fb6482f0827d084c335760e2ef77dc6c9a325b9e

src/base/file_search.cpp
@@ -61,11 +61,9 @@ bool FileSearchHelper::Search(const std::wstring& root,
 
     // Directory
     if (IsDirectory(data)) {
-      if (skip_directories_)
-        continue;
       if (!IsValidDirectory(data))
         continue;
-      if (OnDirectoryFunc)
+      if (!skip_directories_ && OnDirectoryFunc)
         result = OnDirectoryFunc(root, data.cFileName, data);
       if (!skip_subdirectories_ && !result)
         result = Search(AddTrailingSlash(root) + data.cFileName,