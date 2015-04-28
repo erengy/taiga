erengy/taiga

commit 651af401b19cf523aa6fc1a3474936f8bb061404

Decrease memory consumption of anime database

src/library/anime_item.cpp
@@ -186,8 +186,11 @@ void Item::SetId(const std::wstring& id, enum_t service) {
 }
 
 void Item::SetSlug(const std::wstring& slug) {
-  if (metadata_.resource.size() < 2)
+  if (metadata_.resource.size() < 2) {
+    if (slug.empty())
+      return;
     metadata_.resource.resize(2);
+  }
 
   metadata_.resource.at(1) = slug;
 }
@@ -201,8 +204,11 @@ void Item::SetType(int type) {
 }
 
 void Item::SetEpisodeCount(int number) {
-  if (metadata_.extent.size() < 1)
+  if (metadata_.extent.size() < 1) {
+    if (number <= 0)
+      return;
     metadata_.extent.resize(1);
+  }
 
   metadata_.extent.at(0) = number;
 
@@ -213,8 +219,11 @@ void Item::SetEpisodeCount(int number) {
 }
 
 void Item::SetEpisodeLength(int number) {
-  if (metadata_.extent.size() < 2)
+  if (metadata_.extent.size() < 2) {
+    if (number <= 0)
+      return;
     metadata_.extent.resize(2);
+  }
 
   metadata_.extent.at(1) = number;
 }
@@ -243,6 +252,13 @@ void Item::SetEnglishTitle(const std::wstring& title) {
   metadata_.alternative.push_back(new_title);
 }
 
+void Item::InsertSynonym(const std::wstring& synonym) {
+  if (synonym == GetTitle())
+    return;
+  metadata_.alternative.push_back(
+      library::Title(library::kTitleTypeSynonym, synonym));
+}
+
 void Item::SetSynonyms(const std::wstring& synonyms) {
   std::vector<std::wstring> temp;
   Split(synonyms, L"; ", temp);
@@ -252,40 +268,47 @@ void Item::SetSynonyms(const std::wstring& synonyms) {
 }
 
 void Item::SetSynonyms(const std::vector<std::wstring>& synonyms) {
+  if (synonyms.empty() && metadata_.alternative.empty())
+    return;
+
   std::vector<library::Title> alternative;
 
   foreach_(it, metadata_.alternative)
     if (it->type != library::kTitleTypeSynonym)
       alternative.push_back(*it);
 
-  foreach_(it, synonyms) {
-    if (*it == GetTitle())
-      continue;
-
-    library::Title new_title(library::kTitleTypeSynonym, *it);
-    alternative.push_back(new_title);
-  }
+  foreach_(it, synonyms)
+    InsertSynonym(*it);
 
   metadata_.alternative = alternative;
 }
 
 void Item::SetDateStart(const Date& date) {
-  if (metadata_.date.size() < 1)
+  if (metadata_.date.size() < 1) {
+    if (!IsValidDate(date))
+      return;
     metadata_.date.resize(1);
+  }
 
   metadata_.date.at(0) = date;
 }
 
 void Item::SetDateEnd(const Date& date) {
-  if (metadata_.date.size() < 2)
+  if (metadata_.date.size() < 2) {
+    if (!IsValidDate(date))
+      return;
     metadata_.date.resize(2);
+  }
 
   metadata_.date.at(1) = date;
 }
 
 void Item::SetImageUrl(const std::wstring& url) {
-  if (metadata_.resource.size() < 1)
+  if (metadata_.resource.size() < 1) {
+    if (url.empty())
+      return;
     metadata_.resource.resize(1);
+  }
 
   metadata_.resource.at(0) = url;
 }
@@ -307,8 +330,11 @@ void Item::SetGenres(const std::vector<std::wstring>& genres) {
 }
 
 void Item::SetPopularity(int popularity) {
-  if (metadata_.community.size() < 2)
+  if (metadata_.community.size() < 2) {
+    if (popularity <= 0)
+      return;
     metadata_.community.resize(2);
+  }
 
   metadata_.community.at(1) = ToWstr(popularity);
 }
@@ -326,8 +352,11 @@ void Item::SetProducers(const std::vector<std::wstring>& producers) {
 }
 
 void Item::SetScore(double score) {
-  if (metadata_.community.size() < 1)
+  if (metadata_.community.size() < 1) {
+    if (score <= 0.0)
+      return;
     metadata_.community.resize(1);
+  }
 
   metadata_.community.at(0) = score > 0.0 ? ToWstr(score) : L"";
 }

src/library/anime_db.cpp
@@ -70,10 +70,9 @@ void Database::ReadDatabaseNode(xml_node& database_node) {
     std::map<enum_t, std::wstring> id_map;
 
     foreach_xmlnode_(id_node, node, L"id") {
-      std::wstring id = id_node.child_value();
-      std::wstring name = id_node.attribute(L"name").as_string();
+      auto name = id_node.attribute(L"name").as_string();
       enum_t service_id = ServiceManager.GetServiceIdByName(name);
-      id_map[service_id] = id;
+      id_map[service_id] = id_node.child_value();
     }
 
     enum_t source = sync::kTaiga;
@@ -92,34 +91,34 @@ void Database::ReadDatabaseNode(xml_node& database_node) {
       }
     }
 
-    Item& item = items[ToInt(id_map[sync::kTaiga])];  // Creates the item if it doesn't exist
+    int id = ToInt(id_map[sync::kTaiga]);
+    Item& item = items[id];  // Creates the item if it doesn't exist
 
     foreach_(it, id_map)
       item.SetId(it->second, it->first);
 
-    std::vector<std::wstring> synonyms;
-    XmlReadChildNodes(node, synonyms, L"synonym");
-
     item.SetSource(source);
-    item.SetSlug(XmlReadStrValue(node, L"slug"));
-
     item.SetTitle(XmlReadStrValue(node, L"title"));
-    item.SetEnglishTitle(XmlReadStrValue(node, L"english"));
-    item.SetSynonyms(synonyms);
     item.SetType(XmlReadIntValue(node, L"type"));
     item.SetAiringStatus(XmlReadIntValue(node, L"status"));
-    item.SetEpisodeCount(XmlReadIntValue(node, L"episode_count"));
-    item.SetEpisodeLength(XmlReadIntValue(node, L"episode_length"));
-    item.SetDateStart(Date(XmlReadStrValue(node, L"date_start")));
-    item.SetDateEnd(Date(XmlReadStrValue(node, L"date_end")));
-    item.SetImageUrl(XmlReadStrValue(node, L"image"));
     item.SetAgeRating(XmlReadIntValue(node, L"age_rating"));
     item.SetGenres(XmlReadStrValue(node, L"genres"));
     item.SetProducers(XmlReadStrValue(node, L"producers"));
-    item.SetScore(ToDouble(XmlReadStrValue(node, L"score")));
-    item.SetPopularity(XmlReadIntValue(node, L"popularity"));
     item.SetSynopsis(XmlReadStrValue(node, L"synopsis"));
     item.SetLastModified(_wtoi64(XmlReadStrValue(node, L"modified").c_str()));
+
+    // This ordering results in less reallocations
+    item.SetEnglishTitle(XmlReadStrValue(node, L"english"));  // alternative
+    foreach_xmlnode_(child_node, node, L"synonym")
+      item.InsertSynonym(child_node.child_value());  // alternative
+    item.SetPopularity(XmlReadIntValue(node, L"popularity"));  // community(1)
+    item.SetScore(ToDouble(XmlReadStrValue(node, L"score")));  // community(0)
+    item.SetDateEnd(Date(XmlReadStrValue(node, L"date_end")));      // date(1)
+    item.SetDateStart(Date(XmlReadStrValue(node, L"date_start")));  // date(0)
+    item.SetEpisodeLength(XmlReadIntValue(node, L"episode_length"));  // extent(1)
+    item.SetEpisodeCount(XmlReadIntValue(node, L"episode_count"));    // extent(0)
+    item.SetSlug(XmlReadStrValue(node, L"slug"));       // resource(1)
+    item.SetImageUrl(XmlReadStrValue(node, L"image"));  // resource(0)
   }
 }
 

src/library/anime_item.h
@@ -75,6 +75,7 @@ public:
   void SetAiringStatus(int status);
   void SetTitle(const std::wstring& title);
   void SetEnglishTitle(const std::wstring& title);
+  void InsertSynonym(const std::wstring& synonym);
   void SetSynonyms(const std::wstring& synonyms);
   void SetSynonyms(const std::vector<std::wstring>& synonyms);
   void SetDateStart(const Date& date);

commit c928ae7eea4e872b51b683cd34548d341f0f2ba0

Fix bug where episode count was not updated
When the episode count of an anime is set to 0 on MAL (e.g. from 38 to 0, because the series is ongoing), Taiga didn't update its local database with the new value.

This bug was introduced in 651af401b19cf523aa6fc1a3474936f8bb061404.

src/library/anime_item.cpp
@@ -205,11 +205,8 @@ void Item::SetType(int type) {
 }
 
 void Item::SetEpisodeCount(int number) {
-  if (metadata_.extent.size() < 1) {
-    if (number <= 0)
-      return;
+  if (metadata_.extent.size() < 1)
     metadata_.extent.resize(1);
-  }
 
   metadata_.extent.at(0) = number;
 