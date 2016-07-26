9e90a23ed3a6e3d3d6a59dc3534db5e29b310b44
reconman <madara.1212@yahoo.de>
2016-07-26 21:26:16 +0200

    Added reddit discussion search to the Search context menu
    Fixes #286

--- src/taiga/action.cpp
+++ src/taiga/action.cpp
@@ -162,6 +162,15 @@ void ExecuteAction(std::wstring action, WPARAM wParam, LPARAM lParam) {
     if (anime_item) {
       std::wstring title = anime_item->GetTitle();
       ReplaceString(body, L"%title%", EncodeUrl(title));
+
+	  int last_watched_episode = anime_item->GetMyLastWatchedEpisode();
+	  if (last_watched_episode == 0) {
+		  //not watched yet, search for general discussion
+		  ReplaceString(body, L"%watched%", L"");
+	  }
+	  else {
+		  ReplaceString(body, L"%watched%", std::to_wstring(last_watched_episode));
+	  }
     }
     ExecuteLink(body);
 
