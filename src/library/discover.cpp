cbe7b0ceb3d0b49eec95288d5a720f8cdd902408
Eren Okka <erenokka@gmail.com>
2016-10-08 05:17:56 +0300

    Activate Fall 2016 anime season

--- src/library/discover.cpp
+++ src/library/discover.cpp
@@ -43,7 +43,7 @@ namespace library {
 
 SeasonDatabase::SeasonDatabase()
     : available_seasons({anime::Season::kWinter, 2011},
-                        {anime::Season::kSummer, 2016}),
+                        {anime::Season::kFall, 2016}),
       remote_location(L"https://raw.githubusercontent.com"
                       L"/erengy/anime-seasons/master/data/") {
 }
