ed6346120c45cce2f308f16d06a04c63eba091a6
Eren Okka <erenokka@gmail.com>
2017-01-08 10:15:12 +0300

    Add Winter 2017 anime season

--- src/library/discover.cpp
+++ src/library/discover.cpp
@@ -42,7 +42,7 @@ namespace library {
 
 SeasonDatabase::SeasonDatabase()
     : available_seasons({anime::Season::kWinter, 2011},
-                        {anime::Season::kFall, 2016}),
+                        {anime::Season::kWinter, 2017}),
       remote_location(L"https://raw.githubusercontent.com"
                       L"/erengy/anime-seasons/master/data/") {
 }
