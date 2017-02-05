56b719aaea793c4645e1c2611eac4b82e5a6088e
Eren Okka <erenokka@gmail.com>
2017-02-05 21:38:31 +0300

    Set MACHINE option to x86 while building libcurl
    The default value for `MACHINE` is supposed to be `x86` according to curl's BUILD.WINDOWS.txt, and I haven't had any issues building it on a 64-bit system so far. But apparently it doesn't work in every case due to how %PROCESSOR_ARCHITECTURE% environment variable behaves.
    
    Reported by @tophf at #294.

--- deps/build.bat
+++ deps/build.bat
@@ -6,11 +6,11 @@ call "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat"
 cd src\curl\winbuild\
 
 echo Building libcurl for Debug configuration...
-nmake /f Makefile.vc mode=static RTLIBCFG=static VC=14 DEBUG=yes
+nmake /f Makefile.vc mode=static RTLIBCFG=static VC=14 MACHINE=x86 DEBUG=yes
 xcopy /s ..\builds\libcurl-vc14-x86-debug-static-ipv6-sspi-winssl\lib ..\..\..\lib\
 
 echo Building libcurl for Release configuration...
-nmake /f Makefile.vc mode=static RTLIBCFG=static VC=14
+nmake /f Makefile.vc mode=static RTLIBCFG=static VC=14 MACHINE=x86
 xcopy /s ..\builds\libcurl-vc14-x86-release-static-ipv6-sspi-winssl\lib ..\..\..\lib\
 
 echo Done!
