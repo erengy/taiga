578f389a49123aed2f4d9ce4264a82232350a829
Eren Okka <erenokka@gmail.com>
2017-04-07 18:42:55 +0300

    Update batch file to build dependencies
    The script now takes the VC version as a command-line argument.

--- deps/build.bat
+++ deps/build.bat
@@ -1,17 +1,37 @@
 @echo off
 
-echo Running vcvarsall.bat...
-call "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat"
+set vc=%1
+if "%vc%"=="15" goto :initialize_environment
+if "%vc%"=="14" goto :initialize_environment
 
+echo Error in script usage. The correct usage is: %~nx0 [version]
+echo [version] can be "15" ^(VS2017^) or "14" ^(VS2015^)
+echo Example: %~nx0 15
+exit /B 1
+
+:initialize_environment
+echo Initializing environment...
+if "%vc%"=="15" (
+  set vcvarsall="%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
+)
+if "%vc%"=="14" (
+  set vcvarsall="%VS140COMNTOOLS%..\..\VC\vcvarsall.bat"
+)
+call %vcvarsall% || (
+  echo Please edit the build script according to your system configuration.
+  exit /B 1
+)
+
+:build_libcurl
 cd src\curl\winbuild\
 
 echo Building libcurl for Debug configuration...
-nmake /f Makefile.vc mode=static RTLIBCFG=static VC=14 MACHINE=x86 DEBUG=yes
-xcopy /s ..\builds\libcurl-vc14-x86-debug-static-ipv6-sspi-winssl\lib ..\..\..\lib\
+nmake /f Makefile.vc mode=static RTLIBCFG=static VC=%vc% MACHINE=x86 DEBUG=yes
+xcopy /s ..\builds\libcurl-vc%vc%-x86-debug-static-ipv6-sspi-winssl\lib ..\..\..\lib\
 
 echo Building libcurl for Release configuration...
-nmake /f Makefile.vc mode=static RTLIBCFG=static VC=14 MACHINE=x86
-xcopy /s ..\builds\libcurl-vc14-x86-release-static-ipv6-sspi-winssl\lib ..\..\..\lib\
+nmake /f Makefile.vc mode=static RTLIBCFG=static VC=%vc% MACHINE=x86
+xcopy /s ..\builds\libcurl-vc%vc%-x86-release-static-ipv6-sspi-winssl\lib ..\..\..\lib\
 
 echo Done!
 pause
