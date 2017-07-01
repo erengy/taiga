de58dc2e450248baee53f8e571ef2098464f5f26
Eren Okka <erenokka@gmail.com>
2017-07-01 10:12:37 +0300

    Use vswhere to locate latest Visual Studio installation
    vswhere is included with Visual Studio 2017 version 15.2 and later.

--- deps/build.bat
+++ deps/build.bat
@@ -1,8 +1,25 @@
 @echo off
 
 set vc=%1
-if "%vc%"=="15" goto :initialize_environment
-if "%vc%"=="14" goto :initialize_environment
+if "%vc%"=="15" (
+  set vcvarsall="%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
+  goto :initialize_environment
+)
+if "%vc%"=="14" (
+  set vcvarsall="%VS140COMNTOOLS%..\..\VC\vcvarsall.bat"
+  goto :initialize_environment
+)
+
+set vswhere="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
+if exist %vswhere% (
+  for /f "usebackq delims=" %%i in (`%vswhere% -latest -property installationPath`) do (
+    set vcvarsall="%%i\VC\Auxiliary\Build\vcvars32.bat"
+  )
+  for /f "usebackq delims=." %%i in (`%vswhere% -latest -property installationVersion`) do (
+    set vc=%%i
+  )
+  goto :initialize_environment
+)
 
 echo Error in script usage. The correct usage is: %~nx0 [version]
 echo [version] can be "15" ^(VS2017^) or "14" ^(VS2015^)
@@ -11,12 +28,6 @@ exit /B 1
 
 :initialize_environment
 echo Initializing environment...
-if "%vc%"=="15" (
-  set vcvarsall="%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
-)
-if "%vc%"=="14" (
-  set vcvarsall="%VS140COMNTOOLS%..\..\VC\vcvarsall.bat"
-)
 call %vcvarsall% || (
   echo Please edit the build script according to your system configuration.
   exit /B 1
