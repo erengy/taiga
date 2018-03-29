erengy/taiga

commit 3f62fe885676f6819c7d66e4fec3735ddf4d5ed0

Convert user ratings to 100-point scale
We need this to be able to support Kitsu's and AniList's rating systems.

Bumped pre-release identifiers to "alpha.2" to handle compatibility with previous releases.

src/ui/dlg/dlg_stats.cpp
@@ -74,8 +74,8 @@ INT_PTR StatsDialog::DialogProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPar
         int bar_height = GetTextHeight(dc.Get());
         int bar_max = rect.Width() * 3 / 4;
 
-        for (int i = 10; i > 0; i--) {
-          if (i < 10)
+        for (int i = anime::kUserScoreMax; i > 0; i -= 10) {
+          if (i < anime::kUserScoreMax)
             rect.top += bar_height;
 
           if (Stats.score_distribution[i] > 0.0f) {

res/menu.xml
@@ -135,17 +135,17 @@
 	<!-- Score -->
 	<menu name="EditScore">
 		<!-- Note: Overwritten -->
-		<item name="(0) No Score"     action="EditScore(0)"  radio="1"/>
-		<item name="(1) Appalling"    action="EditScore(1)"  radio="1"/>
-		<item name="(2) Horrible"     action="EditScore(2)"  radio="1"/>
-		<item name="(3) Very Bad"     action="EditScore(3)"  radio="1"/>
-		<item name="(4) Bad"          action="EditScore(4)"  radio="1"/>
-		<item name="(5) Average"      action="EditScore(5)"  radio="1"/>
-		<item name="(6) Fine"         action="EditScore(6)"  radio="1"/>
-		<item name="(7) Good"         action="EditScore(7)"  radio="1"/>
-		<item name="(8) Very Good"    action="EditScore(8)"  radio="1"/>
-		<item name="(9) Great"        action="EditScore(9)"  radio="1"/>
-		<item name="(10) Masterpiece" action="EditScore(10)" radio="1"/>
+		<item name="(0) No Score"     action="EditScore(0)"   radio="1"/>
+		<item name="(1) Appalling"    action="EditScore(10)"  radio="1"/>
+		<item name="(2) Horrible"     action="EditScore(20)"  radio="1"/>
+		<item name="(3) Very Bad"     action="EditScore(30)"  radio="1"/>
+		<item name="(4) Bad"          action="EditScore(40)"  radio="1"/>
+		<item name="(5) Average"      action="EditScore(50)"  radio="1"/>
+		<item name="(6) Fine"         action="EditScore(60)"  radio="1"/>
+		<item name="(7) Good"         action="EditScore(70)"  radio="1"/>
+		<item name="(8) Very Good"    action="EditScore(80)"  radio="1"/>
+		<item name="(9) Great"        action="EditScore(90)"  radio="1"/>
+		<item name="(10) Masterpiece" action="EditScore(100)" radio="1"/>
 	</menu>
 	<!-- Status -->
 	<menu name="EditStatus">

src/compat/anime_db.cpp
@@ -77,6 +77,28 @@ void Database::HandleCompatibility(const std::wstring& meta_version) {
   }
 }
 
+void Database::HandleListCompatibility(const std::wstring& meta_version) {
+  const semaver::Version version(WstrToStr(meta_version));
+  bool need_to_save = false;
+
+  // Convert from 10-point scale to 100-point scale
+  if (version < semaver::Version(1, 3, 0, "alpha.2")) {
+    for (auto& pair : items) {
+      auto& item = pair.second;
+      const auto score = item.GetMyScore(false);
+      if (0 < score && score <= 10) {
+        item.SetMyScore(score * 10);
+        need_to_save = true;
+        LOGW(L"Converted score of " + ToWstr(item.GetId()) +
+             L" from " + ToWstr(score) + L" to " + ToWstr(item.GetMyScore(false)));
+      }
+    }
+  }
+
+  if (need_to_save)
+    SaveList();
+}
+
 void Database::ReadDatabaseInCompatibilityMode(xml_document& document) {
   xml_node animedb_node = document.child(L"animedb");
 

src/compat/history.cpp
@@ -16,6 +16,8 @@
 ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <semaver/src/semaver.hpp>
+
 #include "base/foreach.h"
 #include "base/log.h"
 #include "base/string.h"
@@ -23,6 +25,29 @@
 #include "library/anime_db.h"
 #include "library/history.h"
 #include "sync/sync.h"
+
+void History::HandleCompatibility(const std::wstring& meta_version) {
+  const semaver::Version version(WstrToStr(meta_version));
+  bool need_to_save = false;
+
+  // Convert from 10-point scale to 100-point scale
+  if (version < semaver::Version(1, 3, 0, "alpha.2")) {
+    for (auto& item : queue.items) {
+      if (item.score) {
+        const auto score = *item.score;
+        if (0 < score && score <= 10) {
+          item.score = score * 10;
+          need_to_save = true;
+          LOGW(L"Converted score of " + ToWstr(item.anime_id) +
+               L" from " + ToWstr(score) + L" to " + ToWstr(*item.score));
+        }
+      }
+    }
+  }
+
+  if (need_to_save)
+    Save();
+}
 
 void History::ReadQueueInCompatibilityMode(const pugi::xml_document& document) {
   xml_node node_queue = document.child(L"history").child(L"queue");

src/library/anime.h
@@ -58,6 +58,7 @@ enum SeriesType {
 const int kUnknownEpisodeCount = -1;
 const int kUnknownEpisodeLength = -1;
 const double kUnknownScore = 0.0;
+const int kUserScoreMax = 100;
 
 enum MyStatus {
   kMyStatusFirst = 1,

src/library/anime_db.cpp
@@ -436,6 +436,8 @@ bool Database::LoadList() {
       UpdateItem(anime_item);
     }
 
+    HandleListCompatibility(meta_version);
+
   } else {
     LOGW(L"Reading list in compatibility mode");
     ReadListInCompatibilityMode(document);
@@ -451,7 +453,7 @@ bool Database::SaveList(bool include_database) {
   xml_document document;
 
   xml_node meta_node = document.append_child(L"meta");
-  XmlWriteStrValue(meta_node, L"version", L"1.1");
+  XmlWriteStrValue(meta_node, L"version", StrToWstr(Taiga.version.to_string()).c_str());
 
   if (include_database) {
     xml_node node_database = document.append_child(L"database");

src/library/anime_db.h
@@ -62,6 +62,7 @@ private:
 
   bool CheckOldUserDirectory();
   void HandleCompatibility(const std::wstring& meta_version);
+  void HandleListCompatibility(const std::wstring& meta_version);
   void ReadDatabaseInCompatibilityMode(pugi::xml_document& document);
   void ReadListInCompatibilityMode(pugi::xml_document& document);
 };

src/library/anime_util.cpp
@@ -29,6 +29,7 @@
 #include "library/anime_util.h"
 #include "library/history.h"
 #include "sync/kitsu_util.h"
+#include "sync/myanimelist_util.h"
 #include "sync/sync.h"
 #include "taiga/announce.h"
 #include "taiga/path.h"
@@ -953,13 +954,17 @@ std::wstring TranslateNumber(int value, const std::wstring& default_char) {
 }
 
 std::wstring TranslateMyScore(int value, const std::wstring& default_char) {
+  if (!value)
+    return default_char;
+
   switch (taiga::GetCurrentServiceId()) {
     default:
+      return ToWstr(value);
     case sync::kMyAnimeList:
-      return value > 0 ? ToWstr(value) : default_char;
-
+      return ToWstr(sync::myanimelist::TranslateMyRatingTo(value));
     case sync::kKitsu:
-      return value > 0 ? ToWstr(static_cast<double>(value) / 2.0, 1) : default_char;
+      // TODO: Support all rating systems
+      return ToWstr(static_cast<double>(sync::kitsu::TranslateMyRatingTo(value)) / 2.0, 1);
   }
 }
 
@@ -970,33 +975,43 @@ std::wstring TranslateMyScoreFull(int value) {
       switch (value) {
         default:
         case 0: return L"(0) No Score";
-        case 1: return L"(1) Appalling";
-        case 2: return L"(2) Horrible";
-        case 3: return L"(3) Very Bad";
-        case 4: return L"(4) Bad";
-        case 5: return L"(5) Average";
-        case 6: return L"(6) Fine";
-        case 7: return L"(7) Good";
-        case 8: return L"(8) Very Good";
-        case 9: return L"(9) Great";
-        case 10: return L"(10) Masterpiece";
+        case 10: return L"(1) Appalling";
+        case 20: return L"(2) Horrible";
+        case 30: return L"(3) Very Bad";
+        case 40: return L"(4) Bad";
+        case 50: return L"(5) Average";
+        case 60: return L"(6) Fine";
+        case 70: return L"(7) Good";
+        case 80: return L"(8) Very Good";
+        case 90: return L"(9) Great";
+        case 100: return L"(10) Masterpiece";
       }
       break;
 
     case sync::kKitsu:
+      // TODO: Support all rating systems
       switch (value) {
         default:
-        case 0: return L"\u2605 0.0";
-        case 1: return L"\u2605 0.5";
-        case 2: return L"\u2605 1.0";
-        case 3: return L"\u2605 1.5";
-        case 4: return L"\u2605 2.0";
-        case 5: return L"\u2605 2.5";
-        case 6: return L"\u2605 3.0";
-        case 7: return L"\u2605 3.5";
-        case 8: return L"\u2605 4.0";
-        case 9: return L"\u2605 4.5";
-        case 10: return L"\u2605 5.0";
+        case 0: return L"0.0";
+        case 10: return L"1.0";
+        case 15: return L"1.5";
+        case 20: return L"2.0";
+        case 25: return L"2.5";
+        case 30: return L"3.0";
+        case 35: return L"3.5";
+        case 40: return L"4.0";
+        case 45: return L"4.5";
+        case 50: return L"5.0";
+        case 55: return L"5.5";
+        case 60: return L"6.0";
+        case 65: return L"6.5";
+        case 70: return L"7.0";
+        case 75: return L"7.5";
+        case 80: return L"8.0";
+        case 85: return L"8.5";
+        case 90: return L"9.0";
+        case 95: return L"9.5";
+        case 100: return L"10.0";
       }
       break;
   }

commit a123ab7b4332ad77f185030e970811e16f4fc47f

Fix decimal point of score statistics
Caused after 3f62fe885676f6819c7d66e4fec3735ddf4d5ed0.

Fixes #543.

src/ui/dlg/dlg_stats.cpp
@@ -185,12 +185,12 @@ void StatsDialog::Refresh() {
   text += Stats.life_planned_to_watch + L"\n";
   switch (rating_type) {
     case RatingType::Ten:
-      text += ToWstr(Stats.score_mean, 2) + L"\n";
-      text += ToWstr(Stats.score_deviation, 2);
+      text += ToWstr(Stats.score_mean / 10.0, 2) + L"\n";
+      text += ToWstr(Stats.score_deviation / 10.0, 2);
       break;
     case RatingType::Five:
-      text += ToWstr(Stats.score_mean / 2.0, 2) + L"\n";
-      text += ToWstr(Stats.score_deviation / 2.0, 2);
+      text += ToWstr(Stats.score_mean / 20.0, 2) + L"\n";
+      text += ToWstr(Stats.score_deviation / 20.0, 2);
       break;
   }
   SetDlgItemText(IDC_STATIC_ANIME_STAT1, text.c_str());