0e3146f81ab6bf8f30fb910e44bef84438a7c142
Eren Okka <erenokka@gmail.com>
2018-06-10 10:15:30 +0300

    Add link to Discord server

--- src/ui/dlg/dlg_about.cpp
+++ src/ui/dlg/dlg_about.cpp
@@ -137,8 +137,8 @@ BOOL AboutDialog::OnInitDialog() {
       L"\u2022 {\\field{\\*\\fldinst{HYPERLINK \"https://myanimelist.net/clubs.php?cid=21400\"}}{\\fldrslt{MyAnimeList club}}}\\line "
       L"\u2022 {\\field{\\*\\fldinst{HYPERLINK \"https://kitsu.io/groups/taiga\"}}{\\fldrslt{Kitsu group}}}\\line "
       L"\u2022 {\\field{\\*\\fldinst{HYPERLINK \"https://anilist.co/forum/thread/2846/1\"}}{\\fldrslt{AniList thread}}}\\line "
-      L"\u2022 {\\field{\\*\\fldinst{HYPERLINK \"https://twitter.com/taigaapp\"}}{\\fldrslt{Twitter account}}}\\line "
-      L"\u2022 {\\field{\\*\\fldinst{HYPERLINK \"irc://irc.rizon.net/taiga\"}}{\\fldrslt{IRC channel}}}"
+      L"\u2022 {\\field{\\*\\fldinst{HYPERLINK \"https://discord.gg/yeGNktZ\"}}{\\fldrslt{Discord server}}}\\line "
+      L"\u2022 {\\field{\\*\\fldinst{HYPERLINK \"https://twitter.com/taigaapp\"}}{\\fldrslt{Twitter account}}}"
       L"}";
   rich_edit_.SetTextEx(WstrToStr(text));
 
