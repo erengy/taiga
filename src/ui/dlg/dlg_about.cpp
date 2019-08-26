7e1670d34aaa62ed3d8748c0e980b094cf87fb97
Eren Okka <erenokka@gmail.com>
2019-08-27 00:27:59 +0300

    Update contributors list

--- src/ui/dlg/dlg_about.cpp
+++ src/ui/dlg/dlg_about.cpp
@@ -120,7 +120,7 @@ BOOL AboutDialog::OnInitDialog() {
       L"erengy (Eren Okka)\\line\\par "
       L"\\b Contributors:\\b0\\line "
       L"saka, Diablofan, slevir, LordGravewish, rr-, sunjayc, ConnorKrammer, Soinou, Jiyuu,\\line "
-      L"ryban, tollyx, pavelxdd, gunt3001, synthtech\\line\\par "
+      L"ryban, tollyx, pavelxdd, gunt3001, synthtech, cnguy\\line\\par "
       L"\\b Third-party components:\\b0\\line "
       L"{\\field{\\*\\fldinst{HYPERLINK \"https://github.com/HowardHinnant/date\"}}{\\fldrslt{date " + GetLibraryVersion(kDate) + L"}}}, "
       L"{\\field{\\*\\fldinst{HYPERLINK \"https://github.com/discordapp/discord-rpc\"}}{\\fldrslt{Discord RPC " + GetLibraryVersion(kDiscordRpc) + L"}}}, "
