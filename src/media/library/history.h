48443126cd96adf93f71e540fdfe62c8e624c2bb
Eren Okka <erenokka@gmail.com>
2019-09-17 00:24:14 +0300

    Separate queue from history

--- src/media/library/history.h
+++ src/media/library/history.h
@@ -19,79 +19,16 @@
 #pragma once
 
 #include <string>
-#include <optional>
-#include <queue>
 #include <vector>
 
 #include "base/time.h"
 #include "base/xml.h"
+#include "media/library/queue.h"
 #include "track/episode.h"
-
-enum class QueueSearch {
-  DateStart,
-  DateEnd,
-  Episode,
-  Notes,
-  RewatchedTimes,
-  Rewatching,
-  Score,
-  Status,
-  Tags,
-};
-
-class AnimeValues {
-public:
-  std::optional<int> episode;
-  std::optional<int> status;
-  std::optional<int> score;
-  std::optional<Date> date_start;
-  std::optional<Date> date_finish;
-  std::optional<bool> enable_rewatching;
-  std::optional<int> rewatched_times;
-  std::optional<std::wstring> tags;
-  std::optional<std::wstring> notes;
-};
-
-class HistoryItem : public AnimeValues {
-public:
-  HistoryItem();
-  virtual ~HistoryItem() {}
-
-  bool enabled;
-  int anime_id;
-  int mode;
-  std::wstring reason;
-  std::wstring time;
-};
-
-class History;
-
-class HistoryQueue {
-public:
-  HistoryQueue();
-  ~HistoryQueue() {}
-
-  void Add(HistoryItem& item, bool save = true);
-  void Check(bool automatic = true);
-  void Clear(bool save = true);
-  void Merge(bool save = true);
-  bool IsQueued(int anime_id) const;
-  HistoryItem* FindItem(int anime_id, QueueSearch search_mode);
-  HistoryItem* GetCurrentItem();
-  int GetItemCount();
-  void Remove(int index = -1, bool save = true, bool refresh = true, bool to_history = true);
-  void RemoveDisabled(bool save = true, bool refresh = true);
-
-  size_t index;
-  std::vector<HistoryItem> items;
-  History* history;
-  bool updating;
-};
 
 class History {
 public:
   History();
-  ~History() {}
 
   void Clear(bool save = true);
   bool Load();
@@ -101,7 +38,7 @@ public:
 
   std::vector<HistoryItem> items;
   HistoryQueue queue;
-  int limit;
+  int limit = 0;  // 0 for unlimited
 
 private:
   void ReadQueue(const pugi::xml_document& document);
@@ -111,18 +48,4 @@ private:
   std::wstring TranslateModeToString(int mode);
 };
 
-class ConfirmationQueue {
-public:
-  ConfirmationQueue();
-  virtual ~ConfirmationQueue() {}
-
-  void Add(const anime::Episode& episode);
-  void Process();
-
-private:
-  bool in_process_;
-  std::queue<anime::Episode> queue_;
-};
-
-extern class History History;
-extern class ConfirmationQueue ConfirmationQueue;
+inline class History History;
---
+++ src/media/library/queue.h
@@ -0,0 +1,98 @@
+/*
+** Taiga
+** Copyright (C) 2010-2019, Eren Okka
+**
+** This program is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include <string>
+#include <optional>
+#include <queue>
+#include <vector>
+
+#include "base/time.h"
+#include "base/xml.h"
+#include "track/episode.h"
+
+enum class QueueSearch {
+  DateStart,
+  DateEnd,
+  Episode,
+  Notes,
+  RewatchedTimes,
+  Rewatching,
+  Score,
+  Status,
+  Tags,
+};
+
+class AnimeValues {
+public:
+  std::optional<int> episode;
+  std::optional<int> status;
+  std::optional<int> score;
+  std::optional<Date> date_start;
+  std::optional<Date> date_finish;
+  std::optional<bool> enable_rewatching;
+  std::optional<int> rewatched_times;
+  std::optional<std::wstring> tags;
+  std::optional<std::wstring> notes;
+};
+
+class HistoryItem : public AnimeValues {
+public:
+  bool enabled = true;
+  int anime_id = 0;
+  int mode = 0;
+  std::wstring reason;
+  std::wstring time;
+};
+
+class History;
+
+class HistoryQueue {
+public:
+  void Add(HistoryItem& item, bool save = true);
+  void Check(bool automatic = true);
+  void Clear(bool save = true);
+  void Merge(bool save = true);
+  bool IsQueued(int anime_id) const;
+  HistoryItem* FindItem(int anime_id, QueueSearch search_mode);
+  HistoryItem* GetCurrentItem();
+  int GetItemCount();
+  void Remove(int index = -1, bool save = true, bool refresh = true, bool to_history = true);
+  void RemoveDisabled(bool save = true, bool refresh = true);
+
+  size_t index = 0;
+  std::vector<HistoryItem> items;
+  History* history = nullptr;
+  bool updating = false;
+};
+
+class ConfirmationQueue {
+public:
+  ConfirmationQueue();
+  virtual ~ConfirmationQueue() {}
+
+  void Add(const anime::Episode& episode);
+  void Process();
+
+private:
+  bool in_process_ = false;
+  std::queue<anime::Episode> queue_;
+};
+
+inline class ConfirmationQueue ConfirmationQueue;
