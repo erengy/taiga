erengy/taiga

commit f39137281a417c07c60d1b6b998273594bc5654c

Make list status enum class

src/ui/dlg/dlg_season.cpp
@@ -581,8 +581,8 @@ void SeasonDialog::RefreshList(bool redraw_only) {
       }
       break;
     case kSeasonGroupByListStatus:
-      for (int i = anime::kNotInList; i <= anime::kPlanToWatch; i++) {
-        list_.InsertGroup(i, ui::TranslateMyStatus(i, false).c_str(), true, false);
+      for (const auto status : anime::kMyStatuses) {
+        list_.InsertGroup(static_cast<int>(status), ui::TranslateMyStatus(status, false).c_str(), true, false);
       }
       break;
     case kSeasonGroupByType:
@@ -623,7 +623,7 @@ void SeasonDialog::RefreshList(bool redraw_only) {
         group = static_cast<int>(anime_item->GetAiringStatus());
         break;
       case kSeasonGroupByListStatus: {
-        group = anime_item->GetMyStatus();
+        group = static_cast<int>(anime_item->GetMyStatus());
         break;
       }
       case kSeasonGroupByType:

src/media/anime.h
@@ -79,15 +79,21 @@ constexpr int kUnknownEpisodeLength = -1;
 constexpr double kUnknownScore = 0.0;
 constexpr int kUserScoreMax = 100;
 
-enum MyStatus {
-  kMyStatusFirst = 1,
-  kNotInList = 0,
-  kWatching,
-  kCompleted,
-  kOnHold,
-  kDropped,
-  kPlanToWatch,
-  kMyStatusLast
+enum class MyStatus {
+  NotInList,
+  Watching,
+  Completed,
+  OnHold,
+  Dropped,
+  PlanToWatch,
+};
+
+constexpr std::array<MyStatus, 5> kMyStatuses{
+  MyStatus::Watching,
+  MyStatus::Completed,
+  MyStatus::OnHold,
+  MyStatus::Dropped,
+  MyStatus::PlanToWatch,
 };
 
 enum class AgeRating {
@@ -139,7 +145,7 @@ struct MyInformation {
   std::wstring id;
   int watched_episodes = 0;
   int score = 0;
-  int status = MyStatus::kNotInList;
+  MyStatus status = MyStatus::NotInList;
   int rewatched_times = 0;
   bool rewatching = false;
   int rewatching_ep = 0;

src/media/anime_db.h
@@ -51,9 +51,9 @@ public:
   bool LoadList();
   bool SaveList(bool include_database = false) const;
 
-  int GetItemCount(int status, bool check_history = true);
+  int GetItemCount(MyStatus status, bool check_history = true);
 
-  void AddToList(int anime_id, int status);
+  void AddToList(int anime_id, MyStatus status);
   void ClearUserData();
   bool DeleteListItem(int anime_id);
   void UpdateItem(const library::QueueItem& queue_item);

src/media/anime_item.cpp
@@ -304,9 +304,9 @@ int Item::GetMyScore(bool check_queue) const {
   return queue_item ? *queue_item->score : my_info_->score;
 }
 
-int Item::GetMyStatus(bool check_queue) const {
+MyStatus Item::GetMyStatus(bool check_queue) const {
   if (!my_info_.get())
-    return kNotInList;
+    return MyStatus::NotInList;
 
   library::QueueItem* queue_item = check_queue ?
       SearchQueue(library::QueueSearch::Status) : nullptr;
@@ -408,7 +408,7 @@ void Item::SetMyScore(int score) {
   my_info_->score = score;
 }
 
-void Item::SetMyStatus(int status) {
+void Item::SetMyStatus(MyStatus status) {
   assert(my_info_.get());
 
   my_info_->status = status;
@@ -604,7 +604,7 @@ void Item::AddtoUserList() {
 }
 
 bool Item::IsInList() const {
-  return my_info_.get() && GetMyStatus() != kNotInList;
+  return my_info_.get() && GetMyStatus() != MyStatus::NotInList;
 }
 
 void Item::RemoveFromUserList() {

src/media/anime_item.h
@@ -101,7 +101,7 @@ public:
   const std::wstring& GetMyId() const;
   int GetMyLastWatchedEpisode(bool check_queue = true) const;
   int GetMyScore(bool check_queue = true) const;
-  int GetMyStatus(bool check_queue = true) const;
+  MyStatus GetMyStatus(bool check_queue = true) const;
   int GetMyRewatchedTimes(bool check_queue = true) const;
   bool GetMyRewatching(bool check_queue = true) const;
   int GetMyRewatchingEp() const;
@@ -114,7 +114,7 @@ public:
   void SetMyId(const std::wstring& id);
   void SetMyLastWatchedEpisode(int number);
   void SetMyScore(int score);
-  void SetMyStatus(int status);
+  void SetMyStatus(MyStatus status);
   void SetMyRewatchedTimes(int rewatched_times);
   void SetMyRewatching(bool rewatching);
   void SetMyRewatchingEp(int rewatching_ep);

src/media/anime_util.cpp
@@ -214,7 +214,7 @@ bool StartNewRewatch(int anime_id) {
 
   library::QueueItem queue_item;
   queue_item.anime_id = anime_item->GetId();
-  queue_item.status = kWatching;
+  queue_item.status = MyStatus::Watching;
   queue_item.enable_rewatching = true;
   queue_item.episode = 0;
   library::queue.Add(queue_item);
@@ -318,7 +318,7 @@ bool IsUpdateAllowed(const Item& item, const Episode& episode, bool ignore_updat
       return false;
   }
 
-  if (item.GetMyStatus() == kCompleted && !item.GetMyRewatching())
+  if (item.GetMyStatus() == MyStatus::Completed && !item.GetMyRewatching())
     return false;
 
   int number = GetEpisodeHigh(episode);
@@ -364,7 +364,7 @@ void AddToQueue(const Item& item, const Episode& episode, bool change_status) {
     queue_item.date_finish = GetDate();
 
   // Set update mode
-  if (item.GetMyStatus() == kNotInList) {
+  if (item.GetMyStatus() == MyStatus::NotInList) {
     queue_item.mode = library::QueueItemMode::Add;
     change_status = true;
   } else {
@@ -382,11 +382,11 @@ void AddToQueue(const Item& item, const Episode& episode, bool change_status) {
   if (change_status) {
     // Move to completed
     if (item.GetEpisodeCount() == *queue_item.episode && *queue_item.episode > 0) {
-      queue_item.status = kCompleted;
+      queue_item.status = MyStatus::Completed;
     // Move to watching
-    } else if (item.GetMyStatus() != kWatching || *queue_item.episode == 1) {
+    } else if (item.GetMyStatus() != MyStatus::Watching || *queue_item.episode == 1) {
       if (!item.GetMyRewatching()) {
-        queue_item.status = kWatching;
+        queue_item.status = MyStatus::Watching;
       }
     }
   }

src/media/library/export.cpp
@@ -60,14 +60,14 @@ bool ExportAsMalXml(const std::wstring& path) {
     }
   };
 
-  constexpr auto tr_my_status = [](int status) {
+  constexpr auto tr_my_status = [](anime::MyStatus status) {
     switch (status) {
       default:
-      case anime::kWatching: return L"Watching";
-      case anime::kCompleted: return L"Completed";
-      case anime::kOnHold: return L"On-Hold";
-      case anime::kDropped: return L"Dropped";
-      case anime::kPlanToWatch: return L"Plan to Watch";
+      case anime::MyStatus::Watching: return L"Watching";
+      case anime::MyStatus::Completed: return L"Completed";
+      case anime::MyStatus::OnHold: return L"On-Hold";
+      case anime::MyStatus::Dropped: return L"Dropped";
+      case anime::MyStatus::PlanToWatch: return L"Plan to Watch";
     }
   };
 
@@ -90,11 +90,11 @@ bool ExportAsMalXml(const std::wstring& path) {
   XmlWriteStr(node_myinfo, L"user_name", taiga::GetCurrentUsername());
   XmlWriteInt(node_myinfo, L"user_export_type", 1);  // anime
   XmlWriteInt(node_myinfo, L"user_total_anime", count_total_anime());
-  XmlWriteInt(node_myinfo, L"user_total_watching", anime::db.GetItemCount(anime::kWatching));
-  XmlWriteInt(node_myinfo, L"user_total_completed", anime::db.GetItemCount(anime::kCompleted));
-  XmlWriteInt(node_myinfo, L"user_total_onhold", anime::db.GetItemCount(anime::kOnHold));
-  XmlWriteInt(node_myinfo, L"user_total_dropped", anime::db.GetItemCount(anime::kDropped));
-  XmlWriteInt(node_myinfo, L"user_total_plantowatch", anime::db.GetItemCount(anime::kPlanToWatch));
+  XmlWriteInt(node_myinfo, L"user_total_watching", anime::db.GetItemCount(anime::MyStatus::Watching));
+  XmlWriteInt(node_myinfo, L"user_total_completed", anime::db.GetItemCount(anime::MyStatus::Completed));
+  XmlWriteInt(node_myinfo, L"user_total_onhold", anime::db.GetItemCount(anime::MyStatus::OnHold));
+  XmlWriteInt(node_myinfo, L"user_total_dropped", anime::db.GetItemCount(anime::MyStatus::Dropped));
+  XmlWriteInt(node_myinfo, L"user_total_plantowatch", anime::db.GetItemCount(anime::MyStatus::PlanToWatch));
 
   for (const auto& [id, item] : anime::db.items) {
     if (item.IsInList()) {
@@ -129,7 +129,7 @@ bool ExportAsMalXml(const std::wstring& path) {
 }
 
 bool ExportAsMarkdown(const std::wstring& path) {
-  std::map<int, std::vector<std::wstring>> status_lists;
+  std::map<anime::MyStatus, std::vector<std::wstring>> status_lists;
 
   for (const auto& [id, item] : anime::db.items) {
     if (item.IsInList()) {

src/media/library/history.cpp
@@ -99,7 +99,8 @@ void History::ReadQueue(const XmlDocument& document) {
 
     READ_ATTRIBUTE_INT(queue_item.episode, L"episode");
     READ_ATTRIBUTE_INT(queue_item.score, L"score");
-    READ_ATTRIBUTE_INT(queue_item.status, L"status");
+    if (!item.attribute(L"status").empty())
+      queue_item.status = static_cast<anime::MyStatus>(item.attribute(L"status").as_int());
     READ_ATTRIBUTE_BOOL(queue_item.enable_rewatching, L"enable_rewatching");
     READ_ATTRIBUTE_INT(queue_item.rewatched_times, L"rewatched_times");
     READ_ATTRIBUTE_STR(queue_item.tags, L"tags");
@@ -152,7 +153,8 @@ bool History::Save() {
     node_item.append_attribute(L"time") = queue_item.time.c_str();
     APPEND_ATTRIBUTE(L"episode", queue_item.episode);
     APPEND_ATTRIBUTE(L"score", queue_item.score);
-    APPEND_ATTRIBUTE(L"status", queue_item.status);
+    if (queue_item.status)
+      node_item.append_attribute(L"status") = static_cast<int>(*queue_item.status);
     APPEND_ATTRIBUTE(L"enable_rewatching", queue_item.enable_rewatching);
     APPEND_ATTRIBUTE(L"rewatched_times", queue_item.rewatched_times);
     APPEND_ATTRIBUTE_STR(L"tags", queue_item.tags);

commit f99919c052584e3839c2555d68fa3576e33ea8b9

Seasons: Display not-in-list anime
Fixes a regression introduced in f39137281a417c07c60d1b6b998273594bc5654c and moves the group to bottom.

src/ui/dlg/dlg_season.cpp
@@ -49,6 +49,8 @@ enum SeasonToolbarCommand {
   kCommandSeasonView,
 };
 
+constexpr int kNotInListGroupIndex = 1000;
+
 SeasonDialog DlgSeason;
 
 SeasonDialog::SeasonDialog()
@@ -613,13 +615,17 @@ void SeasonDialog::RefreshList(bool redraw_only) {
         list_.InsertGroup(static_cast<int>(status), group.text.c_str(), true, false);
       }
       break;
-    case kSeasonGroupByListStatus:
+    case kSeasonGroupByListStatus: {
       for (const auto status : anime::kMyStatuses) {
         ListGroup group{0, ui::TranslateMyStatus(status, false)};
         groups[static_cast<int>(status)] = group;
         list_.InsertGroup(static_cast<int>(status), group.text.c_str(), true, false);
       }
+      ListGroup group{0, ui::TranslateMyStatus(anime::MyStatus::NotInList, false)};
+      groups[kNotInListGroupIndex] = group;
+      list_.InsertGroup(kNotInListGroupIndex, group.text.c_str(), true, false);
       break;
+    }
     case kSeasonGroupByType:
     default:
       for (const auto type : anime::kSeriesTypes) {
@@ -660,7 +666,10 @@ void SeasonDialog::RefreshList(bool redraw_only) {
         group = static_cast<int>(anime_item->GetAiringStatus());
         break;
       case kSeasonGroupByListStatus: {
-        group = static_cast<int>(anime_item->GetMyStatus());
+        const auto my_status = anime_item->GetMyStatus();
+        group = my_status == anime::MyStatus::NotInList
+                    ? kNotInListGroupIndex
+                    : static_cast<int>(my_status);
         break;
       }
       case kSeasonGroupByType: