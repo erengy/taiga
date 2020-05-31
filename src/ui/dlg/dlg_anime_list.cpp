erengy/taiga

commit f39137281a417c07c60d1b6b998273594bc5654c

Make list status enum class

src/ui/dlg/dlg_anime_list.cpp
@@ -62,7 +62,7 @@ enum AnimeListTooltips {
 AnimeListDialog DlgAnimeList;
 
 AnimeListDialog::AnimeListDialog()
-    : current_status_(anime::kWatching) {
+    : current_status_(anime::MyStatus::Watching) {
 }
 
 BOOL AnimeListDialog::OnInitDialog() {
@@ -94,10 +94,15 @@ BOOL AnimeListDialog::OnInitDialog() {
   listview.InsertColumns();
 
   // Insert tabs and list groups
-  listview.InsertGroup(anime::kNotInList, ui::TranslateMyStatus(anime::kNotInList, false).c_str());
-  for (int i = anime::kMyStatusFirst; i < anime::kMyStatusLast; i++) {
-    tab.InsertItem(i - 1, ui::TranslateMyStatus(i, true).c_str(), (LPARAM)i);
-    listview.InsertGroup(i, ui::TranslateMyStatus(i, false).c_str());
+  listview.InsertGroup(
+      static_cast<int>(anime::MyStatus::NotInList),
+      ui::TranslateMyStatus(anime::MyStatus::NotInList, false).c_str());
+  for (const auto status : anime::kMyStatuses) {
+    tab.InsertItem(static_cast<int>(status) - 1,
+                   ui::TranslateMyStatus(status, true).c_str(),
+                   static_cast<LPARAM>(status));
+    listview.InsertGroup(static_cast<int>(status),
+                         ui::TranslateMyStatus(status, false).c_str());
   }
 
   // Track mouse leave event for the list view
@@ -108,8 +113,8 @@ BOOL AnimeListDialog::OnInitDialog() {
   TrackMouseEvent(&tme);
 
   // Refresh
-  RefreshList(anime::kWatching);
-  RefreshTabs(anime::kWatching);
+  RefreshList(anime::MyStatus::Watching);
+  RefreshTabs(anime::MyStatus::Watching);
 
   // Success
   return TRUE;
@@ -185,7 +190,7 @@ INT_PTR AnimeListDialog::DialogProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM 
                           reinterpret_cast<LPARAM>(&anime_ids));
           } else {
             for (const auto& id : anime_ids) {
-              anime::db.AddToList(id, status);
+              anime::db.AddToList(id, static_cast<anime::MyStatus>(status));
             }
           }
           break;
@@ -603,8 +608,8 @@ void AnimeListDialog::ListView::RefreshItem(int index) {
   }
 
   if (columns[kColumnUserProgress].visible && progress_bars_visible) {
-    if (anime_item->GetMyStatus() != anime::kDropped &&
-        (anime_item->GetMyStatus() != anime::kCompleted || anime_item->GetMyRewatching())) {
+    if (anime_item->GetMyStatus() != anime::MyStatus::Dropped &&
+        (anime_item->GetMyStatus() != anime::MyStatus::Completed || anime_item->GetMyRewatching())) {
       if (anime_item->GetMyLastWatchedEpisode() > 0)
         button_visible[0] = true;
       if (anime_item->GetEpisodeCount() > anime_item->GetMyLastWatchedEpisode() ||
@@ -1002,19 +1007,19 @@ void AnimeListDialog::ListView::DrawProgressBar(HDC hdc, RECT* rc, int index,
     } else {
       switch (anime_item.GetMyStatus()) {
         default:
-        case anime::kWatching:
+        case anime::MyStatus::Watching:
           list_progress_type = ui::kListProgressWatching;
           break;
-        case anime::kCompleted:
+        case anime::MyStatus::Completed:
           list_progress_type = ui::kListProgressCompleted;
           break;
-        case anime::kOnHold:
+        case anime::MyStatus::OnHold:
           list_progress_type = ui::kListProgressOnHold;
           break;
-        case anime::kDropped:
+        case anime::MyStatus::Dropped:
           list_progress_type = ui::kListProgressDropped;
           break;
-        case anime::kPlanToWatch:
+        case anime::MyStatus::PlanToWatch:
           list_progress_type = ui::kListProgressPlanToWatch;
           break;
       }
@@ -1129,7 +1134,7 @@ void AnimeListDialog::ListView::DrawProgressText(HDC hdc, RECT* rc,
     dc.SetTextColor(::GetSysColor(COLOR_GRAYTEXT));
   } else if (!anime::IsValidEpisodeNumber(eps_watched, eps_total)) {
     dc.SetTextColor(::GetSysColor(COLOR_HIGHLIGHT));
-  } else if (eps_watched < eps_total && anime_item.GetMyStatus() == anime::kCompleted) {
+  } else if (eps_watched < eps_total && anime_item.GetMyStatus() == anime::MyStatus::Completed) {
     dc.SetTextColor(::GetSysColor(COLOR_HIGHLIGHT));
   }
   dc.DrawText(text.c_str(), text.length(), rcText,
@@ -1297,8 +1302,8 @@ LRESULT AnimeListDialog::OnTabNotify(LPARAM lParam) {
     // Tab select
     case TCN_SELCHANGE: {
       int tab_index = tab.GetCurrentlySelected();
-      int index = static_cast<int>(tab.GetItemParam(tab_index));
-      RefreshList(index);
+      const auto status = static_cast<anime::MyStatus>(tab.GetItemParam(tab_index));
+      RefreshList(status);
       break;
     }
   }
@@ -1320,7 +1325,7 @@ anime::Item* AnimeListDialog::GetCurrentItem() {
   return anime::db.Find(GetCurrentId());
 }
 
-int AnimeListDialog::GetCurrentStatus() {
+anime::MyStatus AnimeListDialog::GetCurrentStatus() {
   return current_status_;
 }
 
@@ -1335,7 +1340,7 @@ void AnimeListDialog::RebuildIdCache() {
     listview.id_cache[listview.GetItemParam(i)] = i;
 }
 
-void AnimeListDialog::RefreshList(int index) {
+void AnimeListDialog::RefreshList(std::optional<anime::MyStatus> status) {
   if (!IsWindow())
     return;
 
@@ -1343,12 +1348,12 @@ void AnimeListDialog::RefreshList(int index) {
 
   // Remember current position
   int current_position = -1;
-  if (index == -1 && !group_view)
+  if (!status && !group_view)
     current_position = listview.GetTopIndex() + listview.GetCountPerPage() - 1;
 
   // Remember current status
-  if (index > anime::kNotInList)
-    current_status_ = index;
+  if (status && *status != anime::MyStatus::NotInList)
+    current_status_ = *status;
 
   // Disable drawing
   listview.SetRedraw(FALSE);
@@ -1361,7 +1366,7 @@ void AnimeListDialog::RefreshList(int index) {
   listview.EnableGroupView(group_view);
 
   // Add items to list
-  std::vector<int> group_count(anime::kMyStatusLast);
+  std::map<anime::MyStatus, int> group_count;
   int group_index = -1;
   int i = 0;
   for (const auto& [anime_id, anime_item] : anime::db.items) {
@@ -1371,7 +1376,7 @@ void AnimeListDialog::RefreshList(int index) {
       continue;
     if (!group_view) {
       if (anime_item.GetMyRewatching()) {
-        if (current_status_ != anime::kWatching)
+        if (current_status_ != anime::MyStatus::Watching)
           continue;
       } else if (current_status_ != anime_item.GetMyStatus()) {
         continue;
@@ -1380,8 +1385,8 @@ void AnimeListDialog::RefreshList(int index) {
     if (!DlgMain.search_bar.filters.CheckItem(anime_item, kSidebarItemAnimeList))
       continue;
 
-    group_count.at(anime_item.GetMyStatus())++;
-    group_index = group_view ? anime_item.GetMyStatus() : -1;
+    group_count[anime_item.GetMyStatus()]++;
+    group_index = group_view ? static_cast<int>(anime_item.GetMyStatus()) : -1;
     i = listview.GetItemCount();
 
     listview.InsertItem(i, group_index, -1,
@@ -1396,9 +1401,9 @@ void AnimeListDialog::RefreshList(int index) {
 
   // Set group headers
   if (group_view) {
-    for (int i = anime::kMyStatusFirst; i < anime::kMyStatusLast; i++) {
-      std::wstring text = ui::TranslateMyStatus(i, false);
-      text += group_count.at(i) > 0 ? L" (" + ToWstr(group_count.at(i)) + L")" : L"";
+    for (const auto status : anime::kMyStatuses) {
+      std::wstring text = ui::TranslateMyStatus(status, false);
+      text += group_count[status] > 0 ? L" ({})"_format(group_count[status]) : L"";
       listview.SetGroupText(i, text.c_str());
     }
   }
@@ -1468,24 +1473,25 @@ void AnimeListDialog::RefreshListItemColumns(int index, const anime::Item& anime
   }
 }
 
-void AnimeListDialog::RefreshTabs(int index) {
+void AnimeListDialog::RefreshTabs(std::optional<anime::MyStatus> status) {
   if (!IsWindow())
     return;
 
   // Remember last index
-  if (index > anime::kNotInList)
-    current_status_ = index;
+  if (status && *status != anime::MyStatus::NotInList)
+    current_status_ = *status;
 
   // Disable drawing
   tab.SetRedraw(FALSE);
 
   // Refresh text
-  for (int i = anime::kMyStatusFirst; i < anime::kMyStatusLast; i++)
-    tab.SetItemText(i - 1, ui::TranslateMyStatus(i, true).c_str());
+  for (const auto status : anime::kMyStatuses) {
+    tab.SetItemText(static_cast<int>(status) - 1, ui::TranslateMyStatus(status, true).c_str());
+  }
 
   // Select related tab
   bool group_view = !DlgMain.search_bar.filters.text[kSidebarItemAnimeList].empty();
-  int tab_index = current_status_;
+  int tab_index = static_cast<int>(current_status_);
   if (group_view) {
     tab_index = -1;
   } else {
@@ -1511,7 +1517,7 @@ void AnimeListDialog::GoToPreviousTab() {
 
   tab.SetCurrentlySelected(tab_index);
 
-  int status = static_cast<int>(tab.GetItemParam(tab_index));
+  const auto status = static_cast<anime::MyStatus>(tab.GetItemParam(tab_index));
   RefreshList(status);
 }
 
@@ -1527,7 +1533,7 @@ void AnimeListDialog::GoToNextTab() {
 
   tab.SetCurrentlySelected(tab_index);
 
-  int status = static_cast<int>(tab.GetItemParam(tab_index));
+  const auto status = static_cast<anime::MyStatus>(tab.GetItemParam(tab_index));
   RefreshList(status);
 }
 

src/media/anime.h
@@ -79,15 +79,21 @@ constexpr int kUnknownEpisodeLength = -1;
 constexpr double kUnknownScore = 0.0;
 constexpr int kUserScoreMax = 100;
 
-enum MyStatus {
-  kMyStatusFirst = 1,
-  kNotInList = 0,
-  kWatching,
-  kCompleted,
-  kOnHold,
-  kDropped,
-  kPlanToWatch,
-  kMyStatusLast
+enum class MyStatus {
+  NotInList,
+  Watching,
+  Completed,
+  OnHold,
+  Dropped,
+  PlanToWatch,
+};
+
+constexpr std::array<MyStatus, 5> kMyStatuses{
+  MyStatus::Watching,
+  MyStatus::Completed,
+  MyStatus::OnHold,
+  MyStatus::Dropped,
+  MyStatus::PlanToWatch,
 };
 
 enum class AgeRating {
@@ -139,7 +145,7 @@ struct MyInformation {
   std::wstring id;
   int watched_episodes = 0;
   int score = 0;
-  int status = MyStatus::kNotInList;
+  MyStatus status = MyStatus::NotInList;
   int rewatched_times = 0;
   bool rewatching = false;
   int rewatching_ep = 0;

src/media/anime_db.h
@@ -51,9 +51,9 @@ public:
   bool LoadList();
   bool SaveList(bool include_database = false) const;
 
-  int GetItemCount(int status, bool check_history = true);
+  int GetItemCount(MyStatus status, bool check_history = true);
 
-  void AddToList(int anime_id, int status);
+  void AddToList(int anime_id, MyStatus status);
   void ClearUserData();
   bool DeleteListItem(int anime_id);
   void UpdateItem(const library::QueueItem& queue_item);

src/media/anime_item.cpp
@@ -304,9 +304,9 @@ int Item::GetMyScore(bool check_queue) const {
   return queue_item ? *queue_item->score : my_info_->score;
 }
 
-int Item::GetMyStatus(bool check_queue) const {
+MyStatus Item::GetMyStatus(bool check_queue) const {
   if (!my_info_.get())
-    return kNotInList;
+    return MyStatus::NotInList;
 
   library::QueueItem* queue_item = check_queue ?
       SearchQueue(library::QueueSearch::Status) : nullptr;
@@ -408,7 +408,7 @@ void Item::SetMyScore(int score) {
   my_info_->score = score;
 }
 
-void Item::SetMyStatus(int status) {
+void Item::SetMyStatus(MyStatus status) {
   assert(my_info_.get());
 
   my_info_->status = status;
@@ -604,7 +604,7 @@ void Item::AddtoUserList() {
 }
 
 bool Item::IsInList() const {
-  return my_info_.get() && GetMyStatus() != kNotInList;
+  return my_info_.get() && GetMyStatus() != MyStatus::NotInList;
 }
 
 void Item::RemoveFromUserList() {

src/media/anime_item.h
@@ -101,7 +101,7 @@ public:
   const std::wstring& GetMyId() const;
   int GetMyLastWatchedEpisode(bool check_queue = true) const;
   int GetMyScore(bool check_queue = true) const;
-  int GetMyStatus(bool check_queue = true) const;
+  MyStatus GetMyStatus(bool check_queue = true) const;
   int GetMyRewatchedTimes(bool check_queue = true) const;
   bool GetMyRewatching(bool check_queue = true) const;
   int GetMyRewatchingEp() const;
@@ -114,7 +114,7 @@ public:
   void SetMyId(const std::wstring& id);
   void SetMyLastWatchedEpisode(int number);
   void SetMyScore(int score);
-  void SetMyStatus(int status);
+  void SetMyStatus(MyStatus status);
   void SetMyRewatchedTimes(int rewatched_times);
   void SetMyRewatching(bool rewatching);
   void SetMyRewatchingEp(int rewatching_ep);

src/media/anime_util.cpp
@@ -214,7 +214,7 @@ bool StartNewRewatch(int anime_id) {
 
   library::QueueItem queue_item;
   queue_item.anime_id = anime_item->GetId();
-  queue_item.status = kWatching;
+  queue_item.status = MyStatus::Watching;
   queue_item.enable_rewatching = true;
   queue_item.episode = 0;
   library::queue.Add(queue_item);
@@ -318,7 +318,7 @@ bool IsUpdateAllowed(const Item& item, const Episode& episode, bool ignore_updat
       return false;
   }
 
-  if (item.GetMyStatus() == kCompleted && !item.GetMyRewatching())
+  if (item.GetMyStatus() == MyStatus::Completed && !item.GetMyRewatching())
     return false;
 
   int number = GetEpisodeHigh(episode);
@@ -364,7 +364,7 @@ void AddToQueue(const Item& item, const Episode& episode, bool change_status) {
     queue_item.date_finish = GetDate();
 
   // Set update mode
-  if (item.GetMyStatus() == kNotInList) {
+  if (item.GetMyStatus() == MyStatus::NotInList) {
     queue_item.mode = library::QueueItemMode::Add;
     change_status = true;
   } else {
@@ -382,11 +382,11 @@ void AddToQueue(const Item& item, const Episode& episode, bool change_status) {
   if (change_status) {
     // Move to completed
     if (item.GetEpisodeCount() == *queue_item.episode && *queue_item.episode > 0) {
-      queue_item.status = kCompleted;
+      queue_item.status = MyStatus::Completed;
     // Move to watching
-    } else if (item.GetMyStatus() != kWatching || *queue_item.episode == 1) {
+    } else if (item.GetMyStatus() != MyStatus::Watching || *queue_item.episode == 1) {
       if (!item.GetMyRewatching()) {
-        queue_item.status = kWatching;
+        queue_item.status = MyStatus::Watching;
       }
     }
   }

src/media/library/export.cpp
@@ -60,14 +60,14 @@ bool ExportAsMalXml(const std::wstring& path) {
     }
   };
 
-  constexpr auto tr_my_status = [](int status) {
+  constexpr auto tr_my_status = [](anime::MyStatus status) {
     switch (status) {
       default:
-      case anime::kWatching: return L"Watching";
-      case anime::kCompleted: return L"Completed";
-      case anime::kOnHold: return L"On-Hold";
-      case anime::kDropped: return L"Dropped";
-      case anime::kPlanToWatch: return L"Plan to Watch";
+      case anime::MyStatus::Watching: return L"Watching";
+      case anime::MyStatus::Completed: return L"Completed";
+      case anime::MyStatus::OnHold: return L"On-Hold";
+      case anime::MyStatus::Dropped: return L"Dropped";
+      case anime::MyStatus::PlanToWatch: return L"Plan to Watch";
     }
   };
 
@@ -90,11 +90,11 @@ bool ExportAsMalXml(const std::wstring& path) {
   XmlWriteStr(node_myinfo, L"user_name", taiga::GetCurrentUsername());
   XmlWriteInt(node_myinfo, L"user_export_type", 1);  // anime
   XmlWriteInt(node_myinfo, L"user_total_anime", count_total_anime());
-  XmlWriteInt(node_myinfo, L"user_total_watching", anime::db.GetItemCount(anime::kWatching));
-  XmlWriteInt(node_myinfo, L"user_total_completed", anime::db.GetItemCount(anime::kCompleted));
-  XmlWriteInt(node_myinfo, L"user_total_onhold", anime::db.GetItemCount(anime::kOnHold));
-  XmlWriteInt(node_myinfo, L"user_total_dropped", anime::db.GetItemCount(anime::kDropped));
-  XmlWriteInt(node_myinfo, L"user_total_plantowatch", anime::db.GetItemCount(anime::kPlanToWatch));
+  XmlWriteInt(node_myinfo, L"user_total_watching", anime::db.GetItemCount(anime::MyStatus::Watching));
+  XmlWriteInt(node_myinfo, L"user_total_completed", anime::db.GetItemCount(anime::MyStatus::Completed));
+  XmlWriteInt(node_myinfo, L"user_total_onhold", anime::db.GetItemCount(anime::MyStatus::OnHold));
+  XmlWriteInt(node_myinfo, L"user_total_dropped", anime::db.GetItemCount(anime::MyStatus::Dropped));
+  XmlWriteInt(node_myinfo, L"user_total_plantowatch", anime::db.GetItemCount(anime::MyStatus::PlanToWatch));
 
   for (const auto& [id, item] : anime::db.items) {
     if (item.IsInList()) {
@@ -129,7 +129,7 @@ bool ExportAsMalXml(const std::wstring& path) {
 }
 
 bool ExportAsMarkdown(const std::wstring& path) {
-  std::map<int, std::vector<std::wstring>> status_lists;
+  std::map<anime::MyStatus, std::vector<std::wstring>> status_lists;
 
   for (const auto& [id, item] : anime::db.items) {
     if (item.IsInList()) {

src/media/library/history.cpp
@@ -99,7 +99,8 @@ void History::ReadQueue(const XmlDocument& document) {
 
     READ_ATTRIBUTE_INT(queue_item.episode, L"episode");
     READ_ATTRIBUTE_INT(queue_item.score, L"score");
-    READ_ATTRIBUTE_INT(queue_item.status, L"status");
+    if (!item.attribute(L"status").empty())
+      queue_item.status = static_cast<anime::MyStatus>(item.attribute(L"status").as_int());
     READ_ATTRIBUTE_BOOL(queue_item.enable_rewatching, L"enable_rewatching");
     READ_ATTRIBUTE_INT(queue_item.rewatched_times, L"rewatched_times");
     READ_ATTRIBUTE_STR(queue_item.tags, L"tags");
@@ -152,7 +153,8 @@ bool History::Save() {
     node_item.append_attribute(L"time") = queue_item.time.c_str();
     APPEND_ATTRIBUTE(L"episode", queue_item.episode);
     APPEND_ATTRIBUTE(L"score", queue_item.score);
-    APPEND_ATTRIBUTE(L"status", queue_item.status);
+    if (queue_item.status)
+      node_item.append_attribute(L"status") = static_cast<int>(*queue_item.status);
     APPEND_ATTRIBUTE(L"enable_rewatching", queue_item.enable_rewatching);
     APPEND_ATTRIBUTE(L"rewatched_times", queue_item.rewatched_times);
     APPEND_ATTRIBUTE_STR(L"tags", queue_item.tags);

commit 88c9e556db141dd3b0c7d3c6d292bd0e527a1184

Fix anime list group headers
The bug was introduced in f39137281a417c07c60d1b6b998273594bc5654c.

See #881.

src/ui/dlg/dlg_anime_list.cpp
@@ -1372,7 +1372,7 @@ void AnimeListDialog::RefreshList(std::optional<anime::MyStatus> status) {
   // Add items to list
   std::map<anime::MyStatus, int> group_count;
   int group_index = -1;
-  int i = 0;
+  int item_index = 0;
   for (const auto& [anime_id, anime_item] : anime::db.items) {
     if (!anime_item.IsInList())
       continue;
@@ -1391,12 +1391,12 @@ void AnimeListDialog::RefreshList(std::optional<anime::MyStatus> status) {
 
     group_count[anime_item.GetMyStatus()]++;
     group_index = group_view ? static_cast<int>(anime_item.GetMyStatus()) : -1;
-    i = listview.GetItemCount();
+    item_index = listview.GetItemCount();
 
-    listview.InsertItem(i, group_index, -1,
+    listview.InsertItem(item_index, group_index, -1,
                         0, nullptr, LPSTR_TEXTCALLBACK,
                         static_cast<LPARAM>(anime_item.GetId()));
-    RefreshListItemColumns(i, anime_item);
+    RefreshListItemColumns(item_index, anime_item);
   }
 
   auto timer = taiga::timers.timer(taiga::kTimerAnimeList);
@@ -1406,9 +1406,9 @@ void AnimeListDialog::RefreshList(std::optional<anime::MyStatus> status) {
   // Set group headers
   if (group_view) {
     for (const auto status : anime::kMyStatuses) {
-      std::wstring text = ui::TranslateMyStatus(status, false);
-      text += group_count[status] > 0 ? L" ({})"_format(group_count[status]) : L"";
-      listview.SetGroupText(i, text.c_str());
+      const auto text = L"{} ({})"_format(ui::TranslateMyStatus(status, false),
+                                          group_count[status]);
+      listview.SetGroupText(static_cast<int>(status), text.c_str());
     }
   }
 