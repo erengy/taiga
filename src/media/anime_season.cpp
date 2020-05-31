erengy/taiga

commit 1d2cf2d457699211e4d193aa1493ac57f9cf2b00

Rewrite anime season class

src/media/anime_season.cpp
@@ -16,157 +16,197 @@
 ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <map>
 #include <regex>
-#include <vector>
+#include <unordered_map>
+
+#include <nstd/string.hpp>
 
 #include "media/anime_season.h"
 
 #include "base/string.h"
-#include "base/time.h"
+#include "sync/service.h"
 
 namespace anime {
 
-Season::Season()
-    : name(kUnknown), year(0) {
+namespace {
+
+constexpr auto UnknownSeason = Season::Name::Unknown;
+constexpr auto Winter = Season::Name::Winter;
+constexpr auto Spring = Season::Name::Spring;
+constexpr auto Summer = Season::Name::Summer;
+constexpr auto Fall = Season::Name::Fall;
+
+bool ShiftedSeasons() {
+  switch (sync::GetCurrentServiceId()) {
+    case sync::ServiceId::MyAnimeList:
+      return false;  // Winter: January, February, March
+    default:
+      return true;  // Winter: December, January, February
+  }
 }
 
-Season::Season(Name name, unsigned short year)
-    : name(name), year(year) {
+Season::Name GetSeasonName(const date::month& month) {
+  if (ShiftedSeasons()) {
+    switch (static_cast<unsigned>(month)) {
+      default: return UnknownSeason;
+      case 12: case  1: case  2: return Winter;
+      case  3: case  4: case  5: return Spring;
+      case  6: case  7: case  8: return Summer;
+      case  9: case 10: case 11: return Fall;
+    }
+  } else {
+    switch (static_cast<unsigned>(month)) {
+      default: return UnknownSeason;
+      case  1: case  2: case  3: return Winter;
+      case  4: case  5: case  6: return Spring;
+      case  7: case  8: case  9: return Summer;
+      case 10: case 11: case 12: return Fall;
+    }
+  }
 }
+
+constexpr Season::Name GetNextSeasonName(const Season::Name name) {
+  switch (name) {
+    default: return UnknownSeason;
+    case Winter: return Spring;
+    case Spring: return Summer;
+    case Summer: return Fall;
+    case Fall:   return Winter;
+  }
+}
+
+constexpr Season::Name GetPreviousSeasonName(const Season::Name name) {
+  switch (name) {
+    default: return UnknownSeason;
+    case Winter: return Fall;
+    case Spring: return Winter;
+    case Summer: return Spring;
+    case Fall:   return Summer;
+  }
+}
+
+std::pair<date::month, date::month> GetSeasonRange(const Season::Name name) {
+  if (ShiftedSeasons()) {
+    switch (name) {
+      default: return {};
+      case Winter: return {date::December,  date::February};
+      case Spring: return {date::March,     date::May};
+      case Summer: return {date::June,      date::August};
+      case Fall:   return {date::September, date::November};
+    }
+  } else {
+    switch (name) {
+      default: return {};
+      case Winter: return {date::January, date::March};
+      case Spring: return {date::April,   date::June};
+      case Summer: return {date::July,    date::September};
+      case Fall:   return {date::October, date::December};
+    }
+  }
+}
+
+}  // namespace
 
 Season::Season(const Date& date) {
-  year = date.year();
+  if (date.month()) {
+    name = GetSeasonName(date::month{date.month()});
+  }
 
-  if (date.month() == 0 || date.month() > 12) {
-    name = kUnknown;
-  } else if (date.month() < 3) {   // Jan-Feb
-    name = kWinter;
-  } else if (date.month() < 6) {   // Mar-May
-    name = kSpring;
-  } else if (date.month() < 9) {   // Jun-Aug
-    name = kSummer;
-  } else if (date.month() < 12) {  // Sep-Nov
-    name = kFall;
-  } else {                       // Dec
-    name = kWinter;
-    year += 1;
+  if (date.year()) {
+    year = date::year{date.year()};
+    if (ShiftedSeasons() && date.month() &&
+        date::month{date.month()} == date::December) {
+      ++year;  // e.g. December 2018 -> Winter 2019
+    }
   }
 }
 
-Season::Season(const std::wstring& str) : Season() {
-  static const std::map<std::wstring, Season::Name> seasons{
-    {L"winter", kWinter},
-    {L"spring", kSpring},
-    {L"summer", kSummer},
-    {L"fall", kFall},
+Season::Season(const std::string& str) {
+  static const std::unordered_map<std::string, Season::Name> seasons{
+    {"winter", Winter},
+    {"spring", Spring},
+    {"summer", Summer},
+    {"fall", Fall}
   };
 
-  static const std::wregex pattern(L"([A-Za-z]+)[ _](\\d{4})");
-  std::wsmatch match_results;
+  static const std::regex pattern{"([A-Za-z]{4,6})[ _](\\d{4})"};
+  std::smatch match_results;
 
   if (std::regex_match(str, match_results, pattern)) {
-    auto it = seasons.find(ToLower_Copy(match_results[1]));
-    if (it != seasons.end())
+    const auto it = seasons.find(nstd::tolower_string(match_results[1]));
+    if (it != seasons.end()) {
       name = it->second;
-    year = ToInt(match_results[2]);
+    }
+    year = date::year{ToInt(match_results[2])};
   }
 }
 
-Season& Season::operator=(const Season& season) {
-  name = season.name;
-  year = season.year;
+////////////////////////////////////////////////////////////////////////////////
 
-  return *this;
+Season::operator bool() const {
+  return name != UnknownSeason && year.ok();
 }
 
 Season& Season::operator++() {
-  switch (name) {
-    case kWinter:
-      name = kSpring;
-      break;
-    case kSpring:
-      name = kSummer;
-      break;
-    case kSummer:
-      name = kFall;
-      break;
-    case kFall:
-      name = kWinter;
-      ++year;
-      break;
+  name = GetNextSeasonName(name);
+
+  if (name == Winter) {
+    ++year;
   }
 
   return *this;
 }
 
 Season& Season::operator--() {
-  switch (name) {
-    case kWinter:
-      name = kFall;
-      --year;
-      break;
-    case kSpring:
-      name = kWinter;
-      break;
-    case kSummer:
-      name = kSpring;
-      break;
-    case kFall:
-      name = kSummer;
-      break;
+  name = GetPreviousSeasonName(name);
+
+  if (name == Fall) {
+    --year;
   }
 
   return *this;
-}
-
-Season::operator bool() const {
-  return name != kUnknown && year > 0;
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-void Season::GetInterval(Date& date_start, Date& date_end) const {
-  static std::map<Name, std::pair<int, int>> interval{
-    {kUnknown, {0, 0}},
-    {kWinter, {12, 2}},
-    {kSpring, {3, 5}},
-    {kSummer, {6, 8}},
-    {kFall, {9, 11}},
-  };
-  static const std::vector<int> days_in_months{
-    31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
-  };
-
-  date_start.set_year(name == kWinter ? year - 1 : year);
-  date_start.set_month(interval[name].first);
-  date_start.set_day(1);
-
-  date_end.set_year(year);
-  date_end.set_month(interval[name].second);
-  date_end.set_day(days_in_months[date_end.month() - 1]);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 int Season::compare(const Season& season) const {
   if (year != season.year) {
-    if (year == 0)
+    if (!year.ok())
       return nstd::cmp::greater;
-    if (season.year == 0)
+    if (!season.year.ok())
       return nstd::cmp::less;
     return year < season.year ? nstd::cmp::less : nstd::cmp::greater;
   }
 
   if (name != season.name) {
-    if (name == Season::kUnknown)
-      return nstd::cmp::greater;
-    if (season.name == Season::kUnknown)
+    if (name == UnknownSeason)
+      return nstd::cmp::greater;  // Unknown seasons are in the future
+    if (season.name == UnknownSeason)
       return nstd::cmp::less;
     return name < season.name ? nstd::cmp::less : nstd::cmp::greater;
   }
 
   return nstd::cmp::equal;
+}
+
+std::pair<DateFull, DateFull> Season::to_date_range() const {
+  if (!(*this)) {
+    return {};
+  }
+
+  const auto months = GetSeasonRange(name);
+
+  return {
+    {
+      ShiftedSeasons() && name == Winter ? year - date::years{1} : year,
+      months.first,
+      date::day{1}
+    },
+    date::year_month_day_last{
+      year,
+      date::month_day_last(months.second)
+    }
+  };
 }
 
 }  // namespace anime

src/media/anime_season.h
@@ -18,42 +18,49 @@
 
 #pragma once
 
+#include <array>
 #include <string>
 
 #include <nstd/compare.hpp>
 
-class Date;
+#include "base/time.h"
 
 namespace anime {
 
-class Season : public nstd::Comparable<Season> {
+class Season final : public nstd::Comparable<Season> {
 public:
-  enum Name {
-    kUnknown,
-    kWinter,
-    kSpring,
-    kSummer,
-    kFall
+  enum class Name {
+    Unknown,
+    Winter,
+    Spring,
+    Summer,
+    Fall
   };
 
-  Season();
-  Season(Name name, unsigned short year);
-  Season(const Date& date);
-  Season(const std::wstring& str);
-  ~Season() {}
-
-  Season& operator = (const Season& season);
-  Season& operator ++ ();
-  Season& operator -- ();
+  Season() = default;
+  explicit Season(Name name, date::year year) : name{name}, year{year} {}
+  explicit Season(const DateFull& date) : Season{Date{date}} {}
+  explicit Season(const Date& date);
+  explicit Season(const std::string& str);
 
   operator bool() const;
 
-  void GetInterval(Date& date_start, Date& date_end) const;
-
-  Name name;
-  unsigned short year;
+  Season& operator++();
+  Season& operator--();
 
   int compare(const Season& season) const override;
+
+  std::pair<DateFull, DateFull> to_date_range() const;
+
+  Name name = Name::Unknown;
+  date::year year;
+};
+
+constexpr std::array<Season::Name, 4> kSeasons{
+  Season::Name::Winter,
+  Season::Name::Spring,
+  Season::Name::Summer,
+  Season::Name::Fall
 };
 
 }  // namespace anime

src/base/time.cpp
@@ -45,6 +45,10 @@ Date::Date(const SYSTEMTIME& st)
     : year_(st.wYear), month_(st.wMonth), day_(st.wDay) {
 }
 
+Date::Date(date::year year, date::month month, date::day day)
+    : year_(year), month_(month), day_(day) {
+}
+
 Date::Date(unsigned short year, unsigned short month, unsigned short day)
     : year_(year), month_(month), day_(day) {
 }

src/base/time.h
@@ -36,6 +36,7 @@ public:
   explicit Date(const std::wstring& date);
   explicit Date(const DateFull& date);
   explicit Date(const SYSTEMTIME& st);
+  explicit Date(date::year year, date::month month, date::day day);
   explicit Date(unsigned short year, unsigned short month, unsigned short day);
 
   Date& operator=(const Date& date);

src/media/anime_season_db.cpp
@@ -64,14 +64,14 @@ void SeasonDatabase::Review(bool hide_nsfw) {
     return;
   }
 
-  Date date_start, date_end;
-  current_season.GetInterval(date_start, date_end);
+  const auto [date_start, date_end] = current_season.to_date_range();
 
   const auto is_within_date_interval =
       [&date_start, &date_end](const Item& anime_item) {
-        const Date& anime_start = anime_item.GetDateStart();
+        const auto& anime_start = anime_item.GetDateStart();
         if (anime_start.year() && anime_start.month())
-          if (date_start <= anime_start && anime_start <= date_end) return true;
+          if (Date{date_start} <= anime_start && anime_start <= Date{date_end})
+            return true;
         return false;
       };
 
@@ -80,7 +80,7 @@ void SeasonDatabase::Review(bool hide_nsfw) {
   };
 
   // Check for invalid items
-  for (size_t i = 0; i < items.size(); i++) {
+  for (size_t i = 0; i < items.size(); ++i) {
     const int anime_id = items.at(i);
     if (const auto anime_item = anime::db.Find(anime_id)) {
       const Date& anime_start = anime_item->GetDateStart();

src/media/anime_util.h
@@ -29,7 +29,6 @@ class Date;
 namespace anime {
 
 class Item;
-class Season;
 
 bool IsValidId(int anime_id);
 

src/sync/kitsu.cpp
@@ -784,13 +784,16 @@ void GetMetadataById(const int id) {
 }
 
 void GetSeason(const anime::Season season, const int page) {
+  const auto season_year = static_cast<int>(season.year);
+  const auto season_name =
+      WstrToStr(ToLower_Copy(ui::TranslateSeasonName(season.name)));
+
   auto request = BuildRequest();
   request.set_target("{}/edge/anime"_format(kBaseUrl));
 
   hypr::Params params{
-      {"filter[season]",
-       WstrToStr(ToLower_Copy(ui::TranslateSeasonName(season.name)))},
-      {"filter[season_year]", ToStr(season.year)},
+      {"filter[season]", season_name},
+      {"filter[season_year]", ToStr(season_year)},
       {"page[offset]", ToStr(page)},
       {"page[limit]", ToStr(kJsonApiMaximumPageSize)}};
 

src/sync/myanimelist.cpp
@@ -525,12 +525,13 @@ void GetMetadataById(const int id) {
 }
 
 void GetSeason(const anime::Season season, const int page_offset) {
+  const auto season_year = static_cast<int>(season.year);
   const auto season_name =
       WstrToStr(ToLower_Copy(ui::TranslateSeasonName(season.name)));
 
   auto request = BuildRequest();
   request.set_target(
-      "{}/anime/season/{}/{}"_format(kBaseUrl, season.year, season_name));
+      "{}/anime/season/{}/{}"_format(kBaseUrl, season_year, season_name));
   request.set_query({
       {"limit", ToStr(kSeasonPageLimit)},
       {"offset", ToStr(page_offset)},

commit 5337c5774cbd97da38a25cba38e32ffe9da7813e

Fix anime season comparison
The bug was introduced in 1d2cf2d457699211e4d193aa1493ac57f9cf2b00.

See #879.

src/media/anime_season.cpp
@@ -144,7 +144,7 @@ Season::Season(const std::string& str) {
 ////////////////////////////////////////////////////////////////////////////////
 
 Season::operator bool() const {
-  return name != UnknownSeason && year.ok();
+  return name != UnknownSeason && static_cast<int>(year);
 }
 
 Season& Season::operator++() {
@@ -171,9 +171,9 @@ Season& Season::operator--() {
 
 int Season::compare(const Season& season) const {
   if (year != season.year) {
-    if (!year.ok())
+    if (!static_cast<int>(year))
       return nstd::cmp::greater;
-    if (!season.year.ok())
+    if (!static_cast<int>(season.year))
       return nstd::cmp::less;
     return year < season.year ? nstd::cmp::less : nstd::cmp::greater;
   }

src/media/anime_season.h
@@ -53,7 +53,7 @@ public:
   std::pair<DateFull, DateFull> to_date_range() const;
 
   Name name = Name::Unknown;
-  date::year year;
+  date::year year{0};
 };
 
 }  // namespace anime