erengy/taiga

commit e48765485dc3872be6a11ace60885c63af3cc49d

Define application mutex name

setup/Taiga.nsi
@@ -4,8 +4,9 @@
 ; ------------------------------------------------------------------------------
 ; General
 
-; Version number
+; Configuration
 !define CONFIG_PATH "..\src\taiga\config.h"
+!searchparse /file "${CONFIG_PATH}" `#define TAIGA_APP_MUTEX ` PRODUCT_MUTEX
 !searchparse /file "${CONFIG_PATH}" `#define TAIGA_VERSION_MAJOR ` VERSION_MAJOR
 !searchparse /file "${CONFIG_PATH}" `#define TAIGA_VERSION_MINOR ` VERSION_MINOR
 !searchparse /file "${CONFIG_PATH}" `#define TAIGA_VERSION_PATCH ` VERSION_PATCH
@@ -195,7 +196,7 @@ Function CheckInstance
   IntOp $secondsWaited 0 + 0
 
   checkInstanceLoop:
-    System::Call "kernel32::OpenMutex(i 0x100000, b 0, t 'Taiga-33d5a63c-de90-432f-9a8b-f6f733dab258') i .R0"
+    System::Call "kernel32::OpenMutex(i 0x100000, b 0, t '${PRODUCT_MUTEX}') i .R0"
     IntCmp $R0 0 skipInstanceCheck
     System::Call "kernel32::CloseHandle(i $R0)"
 

src/taiga/config.h
@@ -18,11 +18,18 @@
 
 #pragma once
 
+// @NOTE: Make sure to update the following files after changing the names of
+// macros defined here:
+//
+// - /setup/Taiga.nsi
+// - /src/taiga/resource.rc
+
 // Relative path to avoid RC1015 error with version.rc
 #include "../base/preprocessor.h"
 
 #define TAIGA_APP_NAME  L"Taiga"
-#define TAIGA_APP_TITLE L"Taiga"
+#define TAIGA_APP_TITLE L"Taiga"  // @TODO: Remove
+#define TAIGA_APP_MUTEX L"Taiga-33d5a63c-de90-432f-9a8b-f6f733dab258"
 
 #define TAIGA_VERSION_MAJOR 1
 #define TAIGA_VERSION_MINOR 4

src/taiga/taiga.cpp
@@ -96,8 +96,7 @@ BOOL App::InitInstance() {
 
   // Check another instance
   if (!options.allow_multiple_instances) {
-    if (CheckInstance(L"Taiga-33d5a63c-de90-432f-9a8b-f6f733dab258",
-                      L"TaigaMainW")) {
+    if (CheckInstance(TAIGA_APP_MUTEX, L"TaigaMainW")) {
       LOGD(L"Another instance of Taiga is running.");
       return FALSE;
     }

commit 039d4edd47b20d96ca1f39eb2919c0dbf11e48df

Setup: Fix parsing of mutex name
Installer was unable to find and close the running instance, which broke auto-updates.

The bug was introduced in e48765485dc3872be6a11ace60885c63af3cc49d.

setup/Taiga.nsi
@@ -6,7 +6,7 @@
 
 ; Configuration
 !define CONFIG_PATH "..\src\taiga\config.h"
-!searchparse /file "${CONFIG_PATH}" `#define TAIGA_APP_MUTEX ` PRODUCT_MUTEX
+!searchparse /file "${CONFIG_PATH}" `#define TAIGA_APP_MUTEX L"` PRODUCT_MUTEX `"`
 !searchparse /file "${CONFIG_PATH}" `#define TAIGA_VERSION_MAJOR ` VERSION_MAJOR
 !searchparse /file "${CONFIG_PATH}" `#define TAIGA_VERSION_MINOR ` VERSION_MINOR
 !searchparse /file "${CONFIG_PATH}" `#define TAIGA_VERSION_PATCH ` VERSION_PATCH