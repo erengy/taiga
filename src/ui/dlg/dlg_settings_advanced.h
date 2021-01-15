dbb3f1c2fa0df540d917114b23b383ccc40bc50b
Eren Okka <erenokka@gmail.com>
2021-01-15 16:36:58 +0300

    Update license year

--- src/ui/dlg/dlg_settings_advanced.h
+++ src/ui/dlg/dlg_settings_advanced.h
@@ -1,6 +1,6 @@
 /*
 ** Taiga
-** Copyright (C) 2010-2020, Eren Okka
+** Copyright (C) 2010-2021, Eren Okka
 **
 ** This program is free software: you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
