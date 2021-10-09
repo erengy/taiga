2b7a7dc1bc368b4b428821472fae46878ef51ecb
Eren Okka <erenokka@gmail.com>
2021-10-09 15:04:23 +0300

    Fix overlapping text after clearing cache

--- src/ui/dlg/dlg_settings.cpp
+++ src/ui/dlg/dlg_settings.cpp
@@ -477,6 +477,8 @@ void SettingsDialog::RefreshCache() {
   taiga::stats.CalculateLocalData();
   SettingsPage& page = pages[kSettingsPageAdvancedCache];
 
+  page.SetRedraw(FALSE);
+
   // History
   text = ToWstr(library::history.items.size()) + L" item(s)";
   page.SetDlgItemText(IDC_STATIC_CACHE1, text.c_str());
@@ -492,6 +494,10 @@ void SettingsDialog::RefreshCache() {
   // Torrent history
   text = ToWstr(track::aggregator.archive.Size()) + L" item(s)";
   page.SetDlgItemText(IDC_STATIC_CACHE4, text.c_str());
+
+  page.SetRedraw(TRUE);
+  page.RedrawWindow(nullptr, nullptr,
+                    RDW_ERASE | RDW_FRAME | RDW_INVALIDATE | RDW_ALLCHILDREN);
 }
 
 void SettingsDialog::RefreshTorrentFilterList(HWND hwnd_list) {
