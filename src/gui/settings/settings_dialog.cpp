e8a38064fca96d6a4aae75f5ae46f7abd4bb1dd3
Eren Okka <erenokka@gmail.com>
2024-04-28 18:05:28 +0300

    Update settings dialog navigation

--- src/gui/settings/settings_dialog.cpp
+++ src/gui/settings/settings_dialog.cpp
@@ -18,6 +18,7 @@
 
 #include "settings_dialog.hpp"
 
+#include "gui/utils/theme.hpp"
 #include "ui_settings_dialog.h"
 
 #ifdef Q_OS_WINDOWS
@@ -33,9 +34,47 @@ SettingsDialog::SettingsDialog(QWidget* parent) : QDialog(parent), ui_(new Ui::S
   enableMicaBackground(this);
 #endif
 
+  ui_->treeWidget->setIndentation(22);
+
+  const auto add_item = [this](QString icon, QString text) {
+    auto item = new QTreeWidgetItem(ui_->treeWidget, QStringList(text));
+    item->setIcon(0, theme.getIcon(icon));
+    item->setSizeHint(0, QSize{0, 24});
+    return item;
+  };
+
+  const auto add_child = [this](QTreeWidgetItem* parent, QString text) {
+    auto item = new QTreeWidgetItem(parent, QStringList(text));
+  };
+
+  add_item("account_circle", "Accounts");
+  add_item("web_asset", "Application");
+  add_item("list_alt", "Anime List");
+  add_item("folder", "Library");
+  {
+    auto item = add_item("check_circle", "Recognition");
+    add_child(item, "Media players");
+    add_child(item, "Streaming");
+  }
+  {
+    auto item = add_item("share", "Sharing");
+    add_child(item, "Discord");
+    add_child(item, "HTTP");
+    add_child(item, "mIRC");
+  }
+  {
+    auto item = add_item("rss_feed", "Torrents");
+    add_child(item, "Downloads");
+    add_child(item, "Filters");
+  }
+  {
+    auto item = add_item("warning", "Advanced");
+    add_child(item, "Cache");
+  }
+
   ui_->treeWidget->expandAll();
 
-  connect(ui_->treeWidget, &QTreeWidget::currentItemChanged,
+  connect(ui_->treeWidget, &QTreeWidget::currentItemChanged, this,
           [this](QTreeWidgetItem* current, QTreeWidgetItem* previous) {
             if (current) {
               auto text = current->text(0);
