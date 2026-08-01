6fad6988219fdd1a4692402272cce023eb546cbd
Eren Okka <erenokka@gmail.com>
2026-08-01 18:34:45 +0300

    Reset history model on change

--- src/gui/models/history_model.cpp
+++ src/gui/models/history_model.cpp
@@ -1,6 +1,6 @@
 /**
  * Taiga
- * Copyright (C) 2010-2025, Eren Okka
+ * Copyright (C) 2010-2026, Eren Okka
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -24,7 +24,12 @@
 
 namespace gui {
 
-HistoryModel::HistoryModel(QObject* parent) : QAbstractListModel(parent) {}
+HistoryModel::HistoryModel(QObject* parent) : QAbstractListModel(parent) {
+  connect(&anime::history, &anime::History::changed, this, [this]() {
+    beginResetModel();
+    endResetModel();
+  });
+}
 
 int HistoryModel::rowCount(const QModelIndex&) const {
   return anime::history.items().size();
