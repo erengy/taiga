419164808230a72967be4e1c579e66341511f592
Eren Okka <erenokka@gmail.com>
2026-08-02 16:00:12 +0300

    Validate anime type when matching episode

--- src/track/recognition_validate.cpp
+++ src/track/recognition_validate.cpp
@@ -22,7 +22,10 @@
 #include <algorithm>
 #include <anitomy.hpp>
 #include <ranges>
+#include <utility>
+#include <vector>
 
+#include "base/string.hpp"
 #include "media/anime.hpp"
 #include "media/anime_db.hpp"
 #include "track/episode.hpp"
@@ -30,6 +33,46 @@
 namespace track::recognition {
 
 namespace {
+
+anime::Type toAnimeType(const std::string& value) {
+  // This table is supposed to match Anitomy's keywords for `KeywordKind::Type`.
+  static const std::vector<std::pair<std::string, anime::Type>> types{
+      {"tv", anime::Type::Tv},
+      {"movie", anime::Type::Movie},
+      {"gekijouban", anime::Type::Movie},
+      {"oad", anime::Type::Ova},
+      {"oav", anime::Type::Ova},
+      {"ova", anime::Type::Ova},
+      {"ona", anime::Type::Ona},
+      {"sp", anime::Type::Special},
+      {"special", anime::Type::Special},
+      {"specials", anime::Type::Special},
+  };
+
+  const auto it = std::ranges::find_if(types, [&value](const auto& type) {
+    return compareStrings(value, type.first, Qt::CaseInsensitive) == 0;
+  });
+
+  return it != types.end() ? it->second : anime::Type::Unknown;
+}
+
+bool isValidAnimeType(const Episode& episode, const anime::Details& item) {
+  if (item.type == anime::Type::Unknown) {
+    return true;  // anime's type is unknown
+  }
+
+  const auto value = episode.element(anitomy::ElementKind::Type);
+  if (value.empty()) {
+    return true;  // nothing parsed to validate
+  }
+
+  const auto type = toAnimeType(value);
+  if (type == anime::Type::Unknown) {
+    return true;  // not a recognized type keyword
+  }
+
+  return type == item.type;
+}
 
 bool isValidEpisodeNumber(const Episode& episode, const anime::Details& item) {
   if (item.episode_count < 1) {
@@ -68,6 +111,8 @@ bool isValidMatch(const int id, const Episode& episode) {
   const auto item = anime::db.item(id);
 
   if (!item) return false;
+
+  if (!isValidAnimeType(episode, *item)) return false;
 
   if (!isValidEpisodeNumber(episode, *item)) return false;
 
